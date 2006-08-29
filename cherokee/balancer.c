329c0daf7fd15d0ada5b5bf0f86d4ba7893aeaa2
alobbs <alvaro@alobbs.com>
2006-08-29 23:20:02 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@372 5dc97367-97f1-0310-9951-d761b3857238

---
+++ cherokee/balancer.c
@@ -0,0 +1,137 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* Cherokee
+ *
+ * Authors:
+ *      Alvaro Lopez Ortega <alvaro@alobbs.com>
+ *
+ * Copyright (C) 2001-2006 Alvaro Lopez Ortega
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#include "common-internal.h"
+#include "balancer.h"
+
+
+#define DEFAULT_HOSTS_ALLOCATION 5
+
+
+ret_t 
+cherokee_balancer_init_base (cherokee_balancer_t *balancer)
+{
+	/* Init the base class
+	 */
+	cherokee_module_init_base (MODULE(balancer));
+
+	/* Virtual methods
+	 */
+	balancer->dispatch   = NULL;
+	
+	/* Hosts
+	 */
+	balancer->hosts_len  = 0;
+	balancer->hosts_size = 0;
+	balancer->hosts      = NULL;
+
+	return ret_ok;
+}
+
+
+ret_t 
+cherokee_balancer_mrproper (cherokee_balancer_t *balancer)
+{
+	if (balancer->hosts != NULL) {
+		free (balancer->hosts);
+	}
+
+	return ret_ok;
+}
+
+
+static ret_t
+alloc_more_hosts (cherokee_balancer_t *balancer)
+{
+	size_t size;
+
+	if (balancer->hosts == NULL) {
+		size = DEFAULT_HOSTS_ALLOCATION * sizeof(cherokee_balancer_host_t *);
+		balancer->hosts = (cherokee_balancer_host_t **) malloc (size);
+	} else {
+		size = (balancer->hosts_size + DEFAULT_HOSTS_ALLOCATION ) * sizeof(cherokee_balancer_host_t *);
+		balancer->hosts = (cherokee_balancer_host_t **) realloc (balancer->hosts, size);
+	}
+	
+	if (balancer->hosts == NULL) 
+		return ret_nomem;
+	
+	memset (balancer->hosts, 0, size);
+
+	balancer->hosts_size += DEFAULT_HOSTS_ALLOCATION;
+	return ret_ok;
+}
+
+
+ret_t 
+cherokee_balancer_add_host (cherokee_balancer_t *balancer, cherokee_balancer_host_t *host)
+{
+	ret_t ret;
+
+	if (balancer->hosts_len >= balancer->hosts_size) {
+		ret = alloc_more_hosts (balancer);
+		if (ret != ret_ok) return ret;
+	}
+
+	balancer->hosts[balancer->hosts_len] = host;
+	balancer->hosts_len++;
+
+	return ret_ok;
+}
+
+
+ret_t 
+cherokee_balancer_add_dispatch (cherokee_balancer_t *balancer, cherokee_connection_t *conn, cherokee_balancer_host_t **host)
+{
+	if (balancer->dispatch == NULL)
+		return ret_error;
+
+	return balancer->dispatch (balancer, conn, host);
+}
+
+
+ret_t 
+cherokee_balancer_free (cherokee_balancer_t *bal)
+{
+	ret_t              ret;
+	cherokee_module_t *module = MODULE(bal);
+
+	/* Sanity check
+	 */
+	if (module->free == NULL) {
+		return ret_error;
+	}
+	
+	/* Call the virtual method implementation
+	 */
+	ret = MODULE(bal)->free (bal); 
+	if (unlikely (ret < ret_ok)) return ret;
+	
+	/* Free the rest
+	 */
+	cherokee_balancer_mrproper(bal);
+
+	free (bal);
+	return ret_ok;
+}
---
+++ cherokee/balancer.h
@@ -0,0 +1,73 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* Cherokee
+ *
+ * Authors:
+ *      Alvaro Lopez Ortega <alvaro@alobbs.com>
+ *
+ * Copyright (C) 2001-2006 Alvaro Lopez Ortega
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#if !defined (CHEROKEE_INSIDE_CHEROKEE_H) && !defined (CHEROKEE_COMPILATION)
+# error "Only <cherokee/cherokee.h> can be included directly, this file may disappear or change contents."
+#endif
+
+#ifndef CHEROKEE_BALANCER_H
+#define CHEROKEE_BALANCER_H
+
+#include <cherokee/common.h>
+#include <cherokee/module.h>
+#include <cherokee/connection.h>
+
+CHEROKEE_BEGIN_DECLS
+
+
+/* Hosts
+ */
+typedef struct {
+	int foo;
+} cherokee_balancer_host_t;
+
+
+/* Balancer
+ */
+typedef ret_t (* balancer_dispatch_func_t) (void *balancer, cherokee_connection_t *conn, cherokee_balancer_host_t **hosts);
+typedef ret_t (* balancer_free_func_t)     (void *balancer);
+
+typedef struct {
+	cherokee_module_t          module;
+
+	/* Properties */
+	cherokee_balancer_host_t **hosts;
+	cuint_t                    hosts_len;
+	cuint_t                    hosts_size;
+
+	/* Virtual methods */
+	balancer_dispatch_func_t   dispatch;
+} cherokee_balancer_t;
+
+
+ret_t cherokee_balancer_init_base     (cherokee_balancer_t *balancer);
+ret_t cherokee_balancer_mrproper      (cherokee_balancer_t *balancer);
+
+ret_t cherokee_balancer_add_host      (cherokee_balancer_t *balancer, cherokee_balancer_host_t *host);
+ret_t cherokee_balancer_add_dispatch  (cherokee_balancer_t *balancer, cherokee_connection_t *conn, cherokee_balancer_host_t **host);
+ret_t cherokee_balancer_free          (cherokee_balancer_t *balancer);
+
+CHEROKEE_END_DECLS
+
+#endif /* CHEROKEE_BALANCER_H */
