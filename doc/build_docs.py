eb69525524f875681e218a9e9b8ecc1ab9876f5a
Brian Rosner <brosner@gmail.com>
2007-07-11 03:49:45 +0000

    cleaned up some documentation and fixed the build_docs.py without docutils.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@806 5dc97367-97f1-0310-9951-d761b3857238

--- doc/build_docs.py
+++ doc/build_docs.py
@@ -3,56 +3,62 @@
 #       Brian Rosner <brosner@gmail.com>
 #
 # Credit:   Jacob Kaplan-Moss
-#            - For all his hard work on the wonderful Django documentation and              #              is the basis of the Cherokee documentation.
+#            - For all his hard work on the wonderful Django documentation and
+#              is the basis of the Cherokee documentation.
 #
 
 import sys
 import os
 import shutil
 
-from docutils import nodes
-from docutils.core import publish_parts
-from docutils.writers import html4css1
+try:
+    import docutils
+except ImportError:
+    docutils = False
+else:
+    from docutils import nodes
+    from docutils.core import publish_parts
+    from docutils.writers import html4css1
 
 from django.template import Context
 from django.template.loader import get_template
 
-class CherokeeHTMLWriter(html4css1.Writer):
-    
-    def __init__(self):
-        html4css1.Writer.__init__(self)
-        self.translator_class = CherokeeHTMLTranslator
-
-class CherokeeHTMLTranslator(html4css1.HTMLTranslator):
-    
-    # prevent name attributes from being generated
-    named_tags = []
-    
-    def visit_table(self, node):
-        self.body.append(self.starttag(node, 'table', CLASS='docutils'))
-    
-    #
-    # avoid <blockquotes>s around merely indented nodes.
-    # copied from Django project website written by Jacob Kaplan-Moss
-    #
-    
-    _suppress_blockquote_child_nodes = (
-        nodes.bullet_list, nodes.enumerated_list, nodes.definition_list,
-        nodes.literal_block, nodes.doctest_block, nodes.line_block, nodes.table
-    )
-    
-    def _bq_is_valid(self, node):
-        return len(node.children) != 1 or not isinstance(node.children[0], self._suppress_blockquote_child_nodes)
-    
-    def visit_block_quote(self, node):
-        if self._bq_is_valid(node):
-            html4css1.HTMLTranslator.visit_block_quote(self, node)
-    
-    def depart_block_quote(self, node):
-        if self._bq_is_valid(node):
-            html4css1.HTMLTranslator.depart_block_quote(self, node)
-
 def build_document(text):
+    class CherokeeHTMLWriter(html4css1.Writer):
+
+        def __init__(self):
+            html4css1.Writer.__init__(self)
+            self.translator_class = CherokeeHTMLTranslator
+
+    class CherokeeHTMLTranslator(html4css1.HTMLTranslator):
+
+        # prevent name attributes from being generated
+        named_tags = []
+
+        def visit_table(self, node):
+            self.body.append(self.starttag(node, 'table', CLASS='docutils'))
+
+        #
+        # avoid <blockquotes>s around merely indented nodes.
+        # copied from Django project website written by Jacob Kaplan-Moss
+        #
+
+        _suppress_blockquote_child_nodes = (
+            nodes.bullet_list, nodes.enumerated_list, nodes.definition_list,
+            nodes.literal_block, nodes.doctest_block, nodes.line_block, nodes.table
+        )
+
+        def _bq_is_valid(self, node):
+            return len(node.children) != 1 or not isinstance(node.children[0], self._suppress_blockquote_child_nodes)
+
+        def visit_block_quote(self, node):
+            if self._bq_is_valid(node):
+                html4css1.HTMLTranslator.visit_block_quote(self, node)
+
+        def depart_block_quote(self, node):
+            if self._bq_is_valid(node):
+                html4css1.HTMLTranslator.depart_block_quote(self, node)
+    
     return publish_parts(text, writer=CherokeeHTMLWriter())
 
 def main():
@@ -101,7 +107,11 @@ def main():
             data = inf.read()
             inf.close()
             
-            parts = build_document(data)
+            if docutils:
+                parts = build_document(data)
+            else:
+                parts = {'title': 'Unknown', 'body': 'No docutils found.'}
+            
             write_dir = os.path.join(output_dir, d.replace(lang_dir, ''))
             
             if not os.path.exists(write_dir):
