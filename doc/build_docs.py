094046517284f098ff38cd47ad8b89cb19f4fecf
Brian Rosner <brosner@gmail.com>
2007-07-11 01:50:43 +0000

    rewritten documentation system for 0.6.0
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@805 5dc97367-97f1-0310-9951-d761b3857238

---
+++ doc/build_docs.py
@@ -0,0 +1,125 @@
+#
+# Authors:
+#       Brian Rosner <brosner@gmail.com>
+#
+# Credit:   Jacob Kaplan-Moss
+#            - For all his hard work on the wonderful Django documentation and              #              is the basis of the Cherokee documentation.
+#
+
+import sys
+import os
+import shutil
+
+from docutils import nodes
+from docutils.core import publish_parts
+from docutils.writers import html4css1
+
+from django.template import Context
+from django.template.loader import get_template
+
+class CherokeeHTMLWriter(html4css1.Writer):
+    
+    def __init__(self):
+        html4css1.Writer.__init__(self)
+        self.translator_class = CherokeeHTMLTranslator
+
+class CherokeeHTMLTranslator(html4css1.HTMLTranslator):
+    
+    # prevent name attributes from being generated
+    named_tags = []
+    
+    def visit_table(self, node):
+        self.body.append(self.starttag(node, 'table', CLASS='docutils'))
+    
+    #
+    # avoid <blockquotes>s around merely indented nodes.
+    # copied from Django project website written by Jacob Kaplan-Moss
+    #
+    
+    _suppress_blockquote_child_nodes = (
+        nodes.bullet_list, nodes.enumerated_list, nodes.definition_list,
+        nodes.literal_block, nodes.doctest_block, nodes.line_block, nodes.table
+    )
+    
+    def _bq_is_valid(self, node):
+        return len(node.children) != 1 or not isinstance(node.children[0], self._suppress_blockquote_child_nodes)
+    
+    def visit_block_quote(self, node):
+        if self._bq_is_valid(node):
+            html4css1.HTMLTranslator.visit_block_quote(self, node)
+    
+    def depart_block_quote(self, node):
+        if self._bq_is_valid(node):
+            html4css1.HTMLTranslator.depart_block_quote(self, node)
+
+def build_document(text):
+    return publish_parts(text, writer=CherokeeHTMLWriter())
+
+def main():
+    
+    output_dir = './output'
+    
+    #
+    # get the language to compile
+    #
+    try:
+        try:
+            lang = sys.argv[1]
+        except IndexError:
+            lang = 'en'
+    finally:
+        lang_dir = 'locale/%s/' % lang
+        if not os.path.exists(lang_dir):
+            print 'language %s does not exist.' % lang
+            raise SystemExit
+    
+    # delete output_dir if it already exists
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    
+    #
+    # configure django to make templates work
+    #
+    from django.conf import settings
+    settings.configure(**{
+        'TEMPLATE_DEBUG': False,
+        'TEMPLATE_LOADERS': ('django.template.loaders.filesystem.load_template_source',),
+        'TEMPLATE_DIRS': ('templates/',)
+    })
+    
+    doc_detail_template = get_template('doc_detail.html')
+    
+    #
+    # get all the documentation files for the given language
+    #
+    for d, dirs, files in os.walk(lang_dir):
+        
+        media_root = os.path.join(*['..' for i in xrange(len(d.split('/')) - 2) if not d.endswith('/')] + ['media/'])
+        
+        for filename in files:
+            inf = open(os.path.join(d, filename))
+            data = inf.read()
+            inf.close()
+            
+            parts = build_document(data)
+            write_dir = os.path.join(output_dir, d.replace(lang_dir, ''))
+            
+            if not os.path.exists(write_dir):
+                os.mkdir(write_dir)
+            
+            # create the context for django template
+            c = Context({
+                'media_root': media_root,
+                'doc': parts
+            })
+            
+            # write the compiled html doc
+            ouf = open(os.path.join(write_dir, '%s.html' % filename.split('.')[0]), 'w')
+            ouf.write(doc_detail_template.render(c))
+            ouf.close()
+    
+    # move the media directory to the output_dir
+    shutil.copytree('media', os.path.join(output_dir, 'media'))
+
+if __name__ == '__main__':
+    main()
