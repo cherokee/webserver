940a22be260929d37d02237b8a165890b9624191
ADF <adefacc@tin.it>
2007-07-17 21:02:58 +0000

    Skip empty tokens in parse query string.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@843 5dc97367-97f1-0310-9951-d761b3857238

--- cherokee/util.c
+++ cherokee/util.c
@@ -1075,9 +1075,10 @@ cherokee_parse_query_string (cherokee_buffer_t *qstring, cherokee_avl_t *argumen
 	while ((token = (char *) strsep(&string, "&")) != NULL) {
 		char *equ, *key, *val;
 
-		if (token == NULL) continue;
+		if (*token == '\0')
+			continue;
 
-		if ((equ = strchr(token, '='))) {
+		if ((equ = strchr(token, '=')) != NULL) {
 			*equ = '\0';
 
 			key = token;
