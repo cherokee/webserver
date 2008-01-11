96032491c8bf71b06f7ce3925a5efebadc2a9769
alobbs <alvaro@alobbs.com>
2008-01-11 15:39:53 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@979 5dc97367-97f1-0310-9951-d761b3857238

---
+++ admin/ModuleDirlist.py
@@ -0,0 +1,49 @@
+from Form import *
+from Table import *
+from Module import *
+from validations import *
+
+DATA_VALIDATION = [
+    ('^vserver!.+?!.+?!.+?!handler!icon_dir',     validate_path),
+    ('^vserver!.+?!.+?!.+?!handler!notice_files', validate_path_list),
+]
+
+class ModuleDirlist (Module, FormHelper):
+    def __init__ (self, cfg, prefix):
+        Module.__init__ (self, 'dirlist', cfg, prefix)
+        FormHelper.__init__ (self, 'dirlist', cfg)
+
+    def _op_render (self):
+        txt = '<h3>File details</h3>'
+        table = Table(2)
+        self.AddTableCheckbox (table, "Show size", "%s!size" % (self._prefix))
+        self.AddTableCheckbox (table, "Show date", "%s!date" % (self._prefix))
+        self.AddTableCheckbox (table, "Show user", "%s!user" % (self._prefix))
+        self.AddTableCheckbox (table, "Show group", "%s!group" % (self._prefix))
+        txt += str(table)
+        
+        txt += '<h3>Theming</h3>'
+        table = Table(2)
+        self.AddTableEntry    (table, 'Theme name',   "%s!theme" % (self._prefix))
+        self.AddTableEntry    (table, 'Icons dir',    "%s!icon_dir" % (self._prefix))
+        self.AddTableEntry    (table, 'Notice files', "%s!notice_files" % (self._prefix))
+        txt += str(table)
+
+        return txt
+
+    def _op_apply_changes (self, uri, post):
+        self.ValidateChanges (post, DATA_VALIDATION)
+
+        # Text entries
+        for confkey in post:
+            self._cfg[confkey] = post[confkey][0]
+
+        # Rest: Checkboxes
+        for k in ['size', 'date', 'user', 'group']:
+            key = '%s!%s' % (self._prefix, k)
+            if key in post:
+                self._cfg[key] = post[key][0]
+            else:
+                self._cfg[key] = "0"
+                
+        
