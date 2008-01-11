916c3e238be3a19e9885a6dacbc33d01c1a33d1f
alobbs <alvaro@alobbs.com>
2008-01-11 15:57:25 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@980 5dc97367-97f1-0310-9951-d761b3857238

---
+++ admin/ModuleCommon.py
@@ -0,0 +1,17 @@
+from ModuleFile import *
+from ModuleDirlist import *
+
+class ModuleCommon:
+    def __init__ (self, cfg, prefix):
+        self._file = ModuleFile (cfg, prefix)
+        self._dirlist = ModuleDirlist (cfg, prefix)
+
+    def _op_render (self):
+        txt = ''
+        txt += self._file._op_render()
+        txt += self._dirlist._op_render()
+        return txt
+
+    def _op_apply_changes (self, uri, post):
+        self._file._op_apply_changes (uri, post)
+        self._dirlist._op_apply_changes (uri, post)
