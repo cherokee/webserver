bd9c611afb277981d5d621409c57d0769885aab2
alobbs <alvaro@alobbs.com>
2008-02-20 22:31:01 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@1127 5dc97367-97f1-0310-9951-d761b3857238

--- admin/ModuleDirlist.py
+++ admin/ModuleDirlist.py
@@ -1,13 +1,17 @@
+import os
 import validations 
 
 from Form import *
 from Table import *
 from Module import *
+from configured import *
 
 DATA_VALIDATION = [
     ('^vserver!.+?!.+?!.+?!handler!icon_dir',     validations.is_path),
     ('^vserver!.+?!.+?!.+?!handler!notice_files', validations.is_path_list),
 ]
+
+DEFAULT_THEME = "default"
 
 class ModuleDirlist (Module, FormHelper):
     PROPERTIES = [
@@ -29,12 +33,13 @@ class ModuleDirlist (Module, FormHelper):
         self.AddTableCheckbox (table, "Show User",  "%s!user" %(self._prefix), False)
         self.AddTableCheckbox (table, "Show Group", "%s!group"%(self._prefix), False)
         txt += str(table)
-        
+
         txt += '<h3>Theming</h3>'
-        table = Table(2)
-        self.AddTableEntry    (table, 'Theme name',   "%s!theme" % (self._prefix))
-        self.AddTableEntry    (table, 'Icons dir',    "%s!icon_dir" % (self._prefix))
-        self.AddTableEntry    (table, 'Notice files', "%s!notice_files" % (self._prefix))
+        table  = Table(2)
+        themes = self._get_theme_list()
+        self.AddTableOptions (table, 'Theme',        "%s!theme" % (self._prefix), themes)
+        self.AddTableEntry   (table, 'Icons dir',    "%s!icon_dir" % (self._prefix))
+        self.AddTableEntry   (table, 'Notice files', "%s!notice_files" % (self._prefix))
         txt += str(table)
 
         return txt
@@ -42,3 +47,15 @@ class ModuleDirlist (Module, FormHelper):
     def _op_apply_changes (self, uri, post):
         checkboxes = ['size', 'date', 'user', 'group']
         self.ApplyChangesPrefix (self._prefix, checkboxes, post, DATA_VALIDATION)
+
+    def _get_theme_list (self):
+        themes = []
+        for f in os.listdir(CHEROKEE_THEMEDIR):
+            full = os.path.join(CHEROKEE_THEMEDIR, f)
+            if os.path.isdir (full):
+                themes.append((f,f))
+
+        if not themes:
+            themes = [(DEFAULT_THEME, DEFAULT_THEME)]
+
+        return themes
