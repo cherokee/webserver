811fd911e0ef3ddb76a1a7ddbec38cb5ab692fe9
alobbs <alvaro@alobbs.com>
2008-02-23 19:14:28 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@1149 5dc97367-97f1-0310-9951-d761b3857238

--- admin/ModuleFile.py
+++ admin/ModuleFile.py
@@ -4,7 +4,7 @@ from Module import *
 
 class ModuleFile (Module, FormHelper):
     PROPERTIES = [
-        'io_cache'
+        'iocache'
     ]
 
     def __init__ (self, cfg, prefix, submit_url):
@@ -13,9 +13,9 @@ class ModuleFile (Module, FormHelper):
 
     def _op_render (self):
         table = Table(2)
-        self.AddTableCheckbox (table, "Use I/O cache", "%s!io_cache" % (self._prefix), True)
+        self.AddTableCheckbox (table, "Use I/O cache", "%s!iocache" % (self._prefix), True)
         return str(table)
 
     def _op_apply_changes (self, uri, post):
-        self.ApplyChangesPrefix (self._prefix, ['io_cache'], post)
+        self.ApplyChangesPrefix (self._prefix, ['iocache'], post)
 
