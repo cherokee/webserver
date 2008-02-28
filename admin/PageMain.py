c5d61590d439b897da9fdcacc29fe06ec276b7a2
alobbs <alvaro@alobbs.com>
2008-02-28 22:44:26 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@1185 5dc97367-97f1-0310-9951-d761b3857238

--- admin/PageMain.py
+++ admin/PageMain.py
@@ -1,4 +1,8 @@
+import time
+
 from Page import *
+from Table import *
+from configured import *
 from CherokeeManagement import *
 
 SERVER_RUNNING = """
@@ -31,9 +35,10 @@ SERVER_NOT_RUNNING = """
 </div>
 """
 
-class PageMain (PageMenu):
+class PageMain (PageMenu, FormHelper):
     def __init__ (self, cfg=None):
         PageMenu.__init__ (self, 'main', cfg)
+        FormHelper.__init__ (self, 'general', cfg)
 
     def _op_render (self):
         self.AddMacroContent ('title', 'Welcome to Cherokee Admin')
@@ -45,8 +50,47 @@ class PageMain (PageMenu):
         else:
             self.AddMacroContent ('status', SERVER_NOT_RUNNING)
 
+        extra_info = self._render_extra_info()
+        self.AddMacroContent ('extra_info', extra_info)
+
         return Page.Render(self)
 
     def _op_handler (self, uri, post):
         return '/'
     
+    def _render_extra_info (self):
+        txt = ""
+
+        # Server
+        table = Table(2)
+        table += ("Server version", VERSION)
+        table += ("Server prefix",  PREFIX)
+        table += ("Server default path",  WWWROOT)
+
+        manager = cherokee_management_get (self._cfg)
+        if manager._pid:
+            current_pid = str(manager._pid)
+        else:
+            current_pid = "Not running"
+
+        table += ("Server PID",  current_pid)
+
+        txt += '<h3>Server</h3>'
+        txt += self.Indent(table)
+
+        # Configuraion
+        table = Table(2)
+
+        file = self._cfg.file
+        if file:
+            info = os.stat(file)
+            file_status = time.ctime(info.st_ctime)
+            table += ("Configuration file", file)
+            table += ("Configuration modified", file_status)
+        else:
+            table += ("Configuration file", 'Not Found')
+
+        txt += '<h3>Configuration</h3>'
+        txt += self.Indent(table)
+
+        return txt
