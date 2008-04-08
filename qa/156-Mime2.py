322573e1d612f1959c51a512d1b8841860c8069c
alobbs <alvaro@alobbs.com>
2008-04-08 19:24:15 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@1376 5dc97367-97f1-0310-9951-d761b3857238

---
+++ qa/156-Mime2.py
@@ -0,0 +1,26 @@
+from base import *
+
+TYPE       = "example/ejemplo2"
+EXTENSION  = "mime_test_2"
+MAXAGE     = 1234
+
+MIME_TYPES = """
+mime!%s!extensions = %s
+mime!%s!max-age = %s
+""" % (TYPE, EXTENSION, TYPE, MAXAGE)
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self)
+        self.name = "Mime type II: max-age"
+
+        self.request          = "GET /mime2/file.%s HTTP/1.0\r\n" % (EXTENSION)
+        self.expected_error   = 200
+        self.expected_content = ["Content-Type: %s" % (TYPE),
+                                 "Cache-Control: max-age=%s" % (MAXAGE)]
+
+    def Prepare (self, www):
+        d = self.Mkdir (www, "mime2")
+        f = self.WriteFile (d, "file.%s" % (EXTENSION))
+
+        self.conf = MIME_TYPES
