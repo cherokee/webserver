13f49633f84d99ac30cb3f464af801b2c73c65d9
alobbs <alvaro@alobbs.com>
2008-07-07 14:50:56 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@1612 5dc97367-97f1-0310-9951-d761b3857238

---
+++ qa/169-Pipermail.py
@@ -0,0 +1,29 @@
+from base import *
+
+MAGIC      = "This is the content of the HTML index file"
+DIR_LOCAL  = "pipermail169/sub1/sub2"
+DIR_WEB    = "index_droot169"
+DIR_INSIDE = "inside"
+
+CONF = """
+vserver!default!rule!1690!match = directory
+vserver!default!rule!1690!match!directory = /%s
+vserver!default!rule!1690!handler = common
+vserver!default!rule!1690!document_root = %s
+""" 
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self)
+        self.name = "Directory indexer, DocumentRoot"
+
+        self.request          = "GET /%s/%s/ HTTP/1.0\r\n" % (DIR_WEB, DIR_INSIDE)
+        self.expected_error   = 200
+        self.expected_content = MAGIC
+
+    def Prepare (self, www):
+        d = self.Mkdir (www, DIR_LOCAL)
+        self.conf = CONF % (DIR_WEB, d)
+
+        i = self.Mkdir (d, DIR_INSIDE)
+        self.WriteFile (i, "test_index.html", 0444, MAGIC)
