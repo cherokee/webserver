80da18e718e428895a4f69abe9cf5dd7997b9346
taher <taher@shihadeh.net>
2008-07-31 09:48:26 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@1704 5dc97367-97f1-0310-9951-d761b3857238

--- doc/build/build_combined.sh
+++ doc/build/build_combined.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 PACKAGE_VERSION=`grep "PACKAGE_VERSION = " ../Makefile|  sed 's/\(.*\)PACKAGE_VERSION = //'`
 
-python asciidoc.py --conf-file=combined_template.conf --unsafe -a toc -a toclevels=4 -a numbered -a cherokee_version=${PACKAGE_VERSION} -o combined.html combined.txt
-
+python asciidoc.py --conf-file=combined_template.conf --unsafe -a toc -a toclevels=4 -a numbered -a cherokee_version=${PACKAGE_VERSION} -o ../combined.html combined.txt
