84e93a764aa9efa6d499553e5efeec162359a31d
taher <taher@shihadeh.net>
2008-08-14 14:19:11 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@1817 5dc97367-97f1-0310-9951-d761b3857238

--- doc/build/build_web.sh
+++ doc/build/build_web.sh
@@ -6,5 +6,6 @@ ASCIIDOC_HTML="python asciidoc.py --conf-file=web.conf"
 for i in ../*.txt
  do
    FILENAME=`echo $i | sed 's/..\///' | sed 's/\(.*\)txt/\1html/' `
+   echo $FILENAME
    $ASCIIDOC_HTML -o $FILENAME $i
  done
