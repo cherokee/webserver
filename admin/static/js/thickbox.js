b127bceeb466f84f3c60a2799c02c0b0ed5d9eb8
taher <taher@shihadeh.net>
2008-09-26 15:03:10 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2078 5dc97367-97f1-0310-9951-d761b3857238

--- admin/static/js/thickbox.js
+++ admin/static/js/thickbox.js
@@ -5,7 +5,7 @@
  * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
 */
 		  
-var tb_pathToImage = "images/loadingAnimation.gif";
+var tb_pathToImage = "/static/images/loadingAnimation.gif";
 
 /*!!!!!!!!!!!!!!!!! edit below this line at your own risk !!!!!!!!!!!!!!!!!!!!!!!*/
 
