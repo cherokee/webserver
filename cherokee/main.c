201bdf366a13ee5ea748dc60b557bf383f62ce22
alobbs <alvaro@alobbs.com>
2008-10-13 13:12:48 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2167 5dc97367-97f1-0310-9951-d761b3857238

--- cherokee/main.c
+++ cherokee/main.c
@@ -420,7 +420,7 @@ main (int argc, char *argv[])
 	 */
 	ret = check_worker_version (argv[0]);
 	if (ret != ret_ok)
-		exit(1);
+		exit (EXIT_ERROR);
 
 	set_signals();
 	single_time = is_single_execution (argc, argv);
@@ -443,7 +443,7 @@ main (int argc, char *argv[])
 		pid = process_launch (cherokee_worker, argv);
 		if (pid < 0) {
 			PRINT_MSG ("Couldn't launch '%s'\n", cherokee_worker);
-			exit (1);
+			exit (EXIT_ERROR);
 		}
 
 		ret = process_wait (pid);
@@ -455,6 +455,9 @@ main (int argc, char *argv[])
 			DELAY_ERROR);
 	}
 
-	pid_file_clean (pid_file_path);
-	return 0;
+	if (! single_time) {
+		pid_file_clean (pid_file_path);
+	}
+
+	return EXIT_OK;
 }
