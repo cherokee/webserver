c3c5f4ea40ba187bfdfde34ee6a48783730f73ea
alobbs <alvaro@alobbs.com>
2008-11-06 14:54:35 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2344 5dc97367-97f1-0310-9951-d761b3857238

---
+++ qa/187-Method.py
@@ -0,0 +1,44 @@
+from base import *
+
+DIR       = 'method_rule_1'
+MAGIC     = '<a href="http://www.alobbs.com/">Alvaro</a>'
+FORBIDDEN = 'This is forbidden string'
+METHOD    = 'get'
+
+CONF = """
+vserver!1!rule!1871!match = and
+vserver!1!rule!1871!match!left = method
+vserver!1!rule!1871!match!left!method = %s
+vserver!1!rule!1871!match!right = directory
+vserver!1!rule!1871!match!right!directory = /%s
+vserver!1!rule!1871!match!final = 1
+vserver!1!rule!1871!handler = cgi
+
+vserver!1!rule!1870!match = directory
+vserver!1!rule!1870!match!directory = /%s
+vserver!1!rule!1870!match!final = 1
+vserver!1!rule!1870!handler = file
+""" % (METHOD, DIR, DIR)
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self, __file__)
+        self.name              = "Rule Method: match"
+        self.request           = "GET /%s/test HTTP/1.0\r\n" % (DIR)
+        self.expected_content  = MAGIC
+        self.forbidden_content = ['/bin/sh', 'echo', FORBIDDEN]
+        self.expected_error    = 200
+        self.conf              = CONF
+
+    def Prepare (self, www):
+        d = self.Mkdir (www, DIR)
+        self.WriteFile (d, "test", 0555,
+                        """#!/bin/sh
+                            echo "Content-type: text/html"
+                            echo ""
+                            # %s
+                            cat << EOF
+                            %s
+                            EOF
+                            """ % (FORBIDDEN, MAGIC))
+
