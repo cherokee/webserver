6caa1ab6c657f7273c82037759bd3df37d65416e
alobbs <alvaro@alobbs.com>
2008-11-11 17:09:57 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2377 5dc97367-97f1-0310-9951-d761b3857238

---
+++ admin/ModuleProxy.py
@@ -0,0 +1,99 @@
+from Form import *
+from Table import *
+from Module import *
+from validations import *
+from consts import *
+
+from ModuleHandler import *
+from ModuleBalancer import NOTE_BALANCER
+
+NOTE_REUSE_MAX = "Maximum number of connection per server that the proxy can try to keep open."
+
+HELPS = [
+    ('modules_handlers_proxy', "Reverse Proxy")
+]
+
+class ModuleProxy (ModuleHandler):
+    PROPERTIES = [
+        'balancer', 'rewrite_request'
+    ]
+
+    def __init__ (self, cfg, prefix, submit):
+        ModuleHandler.__init__ (self, 'proxy', cfg, prefix, submit)
+        self.show_document_root = False
+
+    def _op_render (self):
+        txt = '<h2>Reverse Proxy specific</h2>'
+
+        # Properties
+        table = TableProps()
+        self.AddPropEntry (table, 'Reuse connections', '%s!reuse_max'%(self._prefix), NOTE_REUSE_MAX)
+        txt += self.Indent (table)
+
+        # URL Rewriting
+        tmp  = ''
+        keys = self._cfg.keys("%s!rewrite_request"%(self._prefix))
+        if keys:
+            tmp += '<h3>URL Rewriting rules</h3>'
+
+            table = Table(3,1, style='width="90%"')
+            table += ('Regular Expression', 'Substitution', '')
+
+            for k in keys:
+                pre = '%s!rewrite_request!%s'%(self._prefix, k)
+                regex = self._cfg.get_val ('%s!regex'%(pre))
+                subst = self._cfg.get_val ('%s!substring'%(pre))
+
+                js      = "post_del_key('/ajax/update', '%s');" % (pre)
+                rm_link = self.InstanceImage ("bin.png", "Delete", border="0", onClick=js)
+                table += (regex, subst, rm_link)
+
+            tmp += self.Indent (table)
+
+            tmp2 = [int(x) for x in keys]
+            tmp2.sort()
+            next = tmp2[-1]+1
+        else:
+            next = 1
+
+        tmp += '<h3>Add new URL Rewrite rules</h3>'
+
+        pre   = "%s!rewrite_request"%(self._prefix)
+        regex = self.InstanceEntry ("%s!%d!regex"%(pre,next),     'text', size=20, req=True)
+        subst = self.InstanceEntry ("%s!%d!substring"%(pre,next), 'text', size=40, req=True)
+        add_b = self.InstanceButton ("Add")
+
+        table  = Table(3,1)
+        table += ('Regular Expression', 'Substitution', '')
+        table += (regex, subst, add_b)
+        tmp += self.Indent(table)
+
+        txt += '<h2>URL Rewriting rules</h2>'
+        txt += self.Indent(tmp)
+                
+        # Balancers
+        table = TableProps()
+        prefix = "%s!balancer" % (self._prefix)
+        e = self.AddPropOptions_Reload (table, "Balancer", prefix, 
+                                        modules_available(BALANCERS), NOTE_BALANCER)
+
+        txt += '<h2>Internal Servers</h2>'
+        txt += self.Indent(str(table) + e)
+        return txt
+
+    def _op_apply_changes (self, uri, post):
+        # Apply balancer changes
+        pre  = "%s!balancer" % (self._prefix)
+
+        new_balancer = post.pop(pre)
+        if new_balancer:
+            self._cfg[pre] = new_balancer
+
+        cfg  = self._cfg[pre]
+        if cfg and cfg.value:
+            name = cfg.value
+            props = module_obj_factory (name, self._cfg, pre, self.submit_url)
+            props._op_apply_changes (uri, post)
+
+        # And CGI changes
+        self.ApplyChangesPrefix (self._prefix, [], post)
--- admin/consts.py
+++ admin/consts.py
@@ -7,6 +7,7 @@ HANDLERS = [
     ('cgi',          'CGI'),
     ('fcgi',         'FastCGI'),
     ('scgi',         'SCGI'),
+    ('proxy',        'HTTP rerverse proxy'),
     ('ssi',          'Server Side Includes'),
     ('server_info',  'Server Info'),
     ('dbslayer',     'MySQL bridge'),
