891c204d6317e671c5633c74f7008ec2fb69de27
alobbs <alvaro@alobbs.com>
2008-11-21 09:57:46 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2443 5dc97367-97f1-0310-9951-d761b3857238

--- qa/conf.py
+++ qa/conf.py
@@ -1,16 +1,28 @@
-PORT     = 1978
-PORT_TLS = 1979
-HOST     = "localhost"
+# Ports
+PORT              = 1978
+PORT_TLS          = 1979
+HOST              = "localhost"
 
+# Basic paths
 CHEROKEE_PATH     = "../cherokee/cherokee-worker"
 CHEROKEE_MODS     = "../cherokee/.libs/"
 CHEROKEE_DEPS     = "../cherokee/"
 CHEROKEE_PANIC    = "../cherokee/cherokee-panic"
 
+# Logging
 LOGGER_TYPE       = "combined"
 LOGGER_ACCESS     = "access.log"
 LOGGER_ERROR      = "error.log"
 
+# TLS/SSL
+SSL_CERT_FILE     = "/etc/cherokee/ssl/cherokee.pem"
+SSL_CERT_KEY_FILE = "/etc/cherokee/ssl/cherokee.pem"
+SSL_CA_FILE       = "/etc/cherokee/ssl/cherokee.pem"
+
+# Misc options
+SERVER_DELAY      = 10
+
+# Third party utilities
 STRACE_PATH       = "/usr/bin/strace"
 DTRUSS_PATH       = "/usr/bin/dtruss"
 VALGRIND_PATH     = "/usr/bin/valgrind"
@@ -18,24 +30,15 @@ PYTHON_PATH       = "auto"
 PHPCGI_PATH       = "auto"
 PHP_FCGI_PORT     = 1980
 
-SSL_CERT_FILE     = "/etc/cherokee/ssl/cherokee.pem"
-SSL_CERT_KEY_FILE = "/etc/cherokee/ssl/cherokee.pem"
-SSL_CA_FILE       = "/etc/cherokee/ssl/cherokee.pem"
-
-SERVER_DELAY      = 10
-
-
-# Paths
-#
 PHP_DIRS          = ["/usr/lib/cgi-bin/",
                      "/usr/bin/",
                      "/usr/local/bin/",
                      "/opt/csw/php5/bin/"]
+
 PYTHON_DIRS       = ["/usr/bin",
                      "/usr/local/bin",
                      "/opt/csw/bin"]
-# Names
-#
+
 PHP_NAMES         = ["php5-cgi",
                      "php-cgi",
                      "php4-cgi"
