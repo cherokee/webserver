fb04cbc1124b2034bfa17f85a153b11542d63896
alobbs <alvaro@alobbs.com>
2008-12-05 09:46:26 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2552 5dc97367-97f1-0310-9951-d761b3857238

--- qa/030-Post+urlencoded.py
+++ qa/030-Post+urlencoded.py
@@ -1,31 +1,35 @@
 import os
 from base import *
 
-CONF = """
+DIR   = "post_zero_len1"
+MAGIC = '<a href="http://www.alobbs.com/">Alvaro</a>'
+
+CONF  = """
 vserver!1!rule!300!match = directory
-vserver!1!rule!300!match!directory = /post2
+vserver!1!rule!300!match!directory = /%s
 vserver!1!rule!300!handler = cgi
-"""
+""" % (DIR)
+
+CGI_BASE = """#!/bin/sh
+echo "Content-Type: text/plain"
+echo
+echo '%s'
+""" % (MAGIC)
 
 class Test (TestBase):
     def __init__ (self):
         TestBase.__init__ (self, __file__)
         self.name = "Post with length zero"
 
-        self.conf           = CONF
-        self.request        = "POST /post2/test HTTP/1.0\r\n" +\
-                              "Content-type: application/x-www-form-urlencoded\r\n" +\
-                              "Content-length: 0\r\n"
-        self.post           = ""
-        self.expected_error = 200
+        self.request          = "POST /%s/test HTTP/1.0\r\n" % (DIR)+\
+                                "Content-type: application/x-www-form-urlencoded\r\n" +\
+                                "Content-length: 0\r\n"
+        self.post             = ""
+        self.conf             = CONF
+        self.expected_error   = 200
+        self.expected_content = MAGIC
 
     def Prepare (self, www):
-        self.Mkdir (www, "post2")
-        self.WriteFile (www, "post2/test", 0755,
-                        """#!/bin/sh
-
-                        echo "Content-Type: text/plain"
-                        echo
-                        echo ok
-                        """)
+        d = self.Mkdir (www, DIR)
+        self.WriteFile (d, "test", 0755, CGI_BASE)
 
