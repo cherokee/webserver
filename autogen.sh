b288ceb93b2984d059a63bb41e5293c435beb343
alobbs <alvaro@alobbs.com>
2009-01-05 11:12:12 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2668 5dc97367-97f1-0310-9951-d761b3857238

--- autogen.sh
+++ autogen.sh
@@ -30,36 +30,38 @@ else
 fi
 
 ($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
+    echo
+    echo "You must have autoconf installed to compile $PROJECT."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    DIE=1
 }
 
 ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have libtool installed to compile $PROJECT."
-        echo "Get ftp://ftp.gnu.org/gnu/libtool/libtool-1.4.tar.gz"
-        echo "(or a newer version if it is available)"
-        DIE=1
+    echo
+    echo "You must have libtool installed to compile $PROJECT."
+    echo "Download the appropriate package for your distribution, or"
+    echo "get http://ftp.gnu.org/gnu/libtool/libtool-2.2.6a.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
 }
 
 ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
+    echo
+    echo "You must have automake installed to compile $PROJECT."
+    echo "Download the appropriate package for your distribution, or"
+    echo "get http://ftp.gnu.org/gnu/automake/automake-1.10.2.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
 }
 
 if test "$DIE" -eq 1; then
-	exit 1
+    exit 1
 fi
 
 if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
+    echo "I am going to run ./configure with no arguments - if you wish "
+    echo "to pass any to it, please specify them on the $0 command line."
 fi
 
 case $CC in
