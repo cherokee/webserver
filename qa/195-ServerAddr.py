724e1eaaaf46c547e613c73c782cbb280b4519c4
alobbs <alvaro@alobbs.com>
2009-01-11 09:18:41 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2692 5dc97367-97f1-0310-9951-d761b3857238

---
+++ qa/195-ServerAddr.py
@@ -0,0 +1,30 @@
+from base import *
+
+DIR = "cgi-bin_serveraddr"
+
+CONF = """
+vserver!1!rule!1950!match = directory
+vserver!1!rule!1950!match!directory = /%s
+vserver!1!rule!1950!handler = cgi
+""" % (DIR)
+
+CGI_BASE = """#!/bin/sh
+echo "Content-Type: text/plain"
+echo
+echo "SERVER_ADDR: $SERVER_ADDR"
+"""
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self, __file__)
+        self.name = "CGI: SERVER_ADDR"
+
+        self.request          = "GET /%s/test HTTP/1.0\r\n" % (DIR)
+        self.conf             = CONF
+        self.expected_content = "127.0.0.1"
+        self.expected_error   = 200
+        self.proxy_suitable   = False
+
+    def Prepare (self, www):
+        self.Mkdir (www, DIR)
+        self.WriteFile (www, "%s/test"%(DIR), 0755, CGI_BASE)
