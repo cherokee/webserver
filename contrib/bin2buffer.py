7422ab7bf6f0a7af0dc0ec00a721fce6d5a29699
alobbs <alvaro@alobbs.com>
2009-01-16 10:01:57 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2713 5dc97367-97f1-0310-9951-d761b3857238

--- contrib/bin2buffer.py
+++ contrib/bin2buffer.py
@@ -1,14 +1,12 @@
-import sys, string
-
-def is_letter(x):
-   if ord(x) in range(71, 91) or ord(x) in range(103, 123):
-      return True
-   return False
-
+import os, sys, string
 
 def print_block(info, len):
     print 'ret = cherokee_buffer_add (buffer, \\\n' + info + ', ' + str(len) + ');'
     print 'if (ret < ret_ok) return ret;\n'
+
+
+print 'ret = cherokee_buffer_ensure_size (buffer, %d);' % (os.path.getsize(sys.argv[1]))
+print 'if (ret < ret_ok) return ret;\n'
 
 file = open (sys.argv[1], "r")
 cont = file.read()
