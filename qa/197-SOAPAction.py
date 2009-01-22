02ca0c4471e92bd144ea66f597e50d5f9477919f
alobbs <alvaro@alobbs.com>
2009-01-22 15:04:34 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2744 5dc97367-97f1-0310-9951-d761b3857238

--- qa/197-SOAPAction.py
+++ qa/197-SOAPAction.py
@@ -5,6 +5,15 @@ HEADER    = "SOAPAction"
 VALUE     = '"urn:wroxheroes"'
 FORBIDDEN = "SOAP headers"
 
+CONF = """
+vserver!1!rule!1970!match = directory
+vserver!1!rule!1970!match!directory = /%s
+vserver!1!rule!1970!handler = fcgi
+vserver!1!rule!1970!handler!balancer = round_robin
+vserver!1!rule!1970!handler!balancer!source!1 = 1
+vserver!1!rule!1970!handler!pass_req_headers = 1
+""" % (DIR)
+
 PHP_SCRIPT = """
 <?php 
    /* %s */
@@ -15,13 +24,15 @@ class Test (TestBase):
     def __init__ (self):
         TestBase.__init__ (self, __file__)
         self.name             = "SOAPAction"
-        self.request          = "GET /%s/test.php HTTP/1.0\r\n" % (DIR)
+        self.request          = "GET /%s/file HTTP/1.0\r\n" % (DIR) + \
+                                "%s: %s\r\n" % (HEADER, VALUE)
         self.expected_error   = 200
         self.expected_content = "%s: %s" % (HEADER, VALUE)
+        self.conf             = CONF
 
     def Prepare (self, www):
         d = self.Mkdir (www, DIR)
-        self.WriteFile (d, "test.php", 0444,
+        self.WriteFile (d, "file", 0444,
                         '<?php /* %s */ echo "%s: ".$_SERVER["HTTP_SOAPACTION"]; ?>' % 
                         (FORBIDDEN, HEADER))
 
