6eaec4577dca1c9772b2a9e94f2963f8c0ba47a6
alobbs <alvaro@alobbs.com>
2009-01-30 18:57:56 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2805 5dc97367-97f1-0310-9951-d761b3857238

--- qa/198-HTTPresponse.py
+++ qa/198-HTTPresponse.py
@@ -1,28 +1,32 @@
 from base import *
 
 DIR = "cgi-bin_httpresponse"
+MSG = "Page eaten by dog"
 
 CONF = """
 vserver!1!rule!1980!match = directory
 vserver!1!rule!1980!match!directory = /%s
 vserver!1!rule!1980!handler = cgi
+vserver!1!rule!1980!handler!error_handler = 0
 """ % (DIR)
 
 CGI_BASE = """#!/bin/sh
 echo "Content-Type: text/plain"
-echo "HTTP/1.0 404 Not Found"
+echo "HTTP/1.0 404 %s" 
+echo "Foo: bar"
 echo
-echo "Sorry. Page eaten by dog."
-"""
+echo "Sorry, file not found."
+""" % (MSG)
 
 class Test (TestBase):
     def __init__ (self):
         TestBase.__init__ (self, __file__)
         self.name = "CGI: HTTP response with reply headers"
 
-        self.request        = "GET /%s/test HTTP/1.0\r\n" % (DIR)
-        self.conf           = CONF
-        self.expected_error = 404
+        self.request           = "GET /%s/test HTTP/1.0\r\n" % (DIR)
+        self.conf              = CONF
+        self.expected_error    = 404
+        self.forbidden_content = MSG
 
     def Prepare (self, www):
         self.Mkdir (www, DIR)
