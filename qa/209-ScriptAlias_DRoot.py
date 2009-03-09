b6fdbd29e2f2bc7786ea51f5e1fcb5ffd70fc81d
alobbs <alvaro@alobbs.com>
2009-03-09 18:53:49 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2949 5dc97367-97f1-0310-9951-d761b3857238

--- qa/209-ScriptAlias_DRoot.py
+++ qa/209-ScriptAlias_DRoot.py
@@ -1,13 +1,16 @@
 import os.path
 from base import *
 
+TOP_DIR     = "209_top_dir"
+DIR         = "209_scripalias_documentroot"
 PATH_INFO   = "/this_is_the/path_info"
 
 CONF = """
-vserver!1!rule!1040!match = directory
-vserver!1!rule!1040!match!directory = /alias_and_pathinfo
-vserver!1!rule!1040!handler = cgi
-vserver!1!rule!1040!handler!script_alias = %s
+vserver!1!rule!2090!match = directory
+vserver!1!rule!2090!match!directory = /%s
+vserver!1!rule!2090!handler = cgi
+vserver!1!rule!2090!handler!script_alias = %s
+vserver!1!rule!2090!document_root = %s
 """
 CGI_BASE = """#!/bin/sh
 echo "Content-Type: text/plain"
@@ -20,11 +23,11 @@ class Test (TestBase):
         TestBase.__init__ (self, __file__)
         self.name = "CGI: ScriptAlias with PathInfo"
 
-        self.request          = "GET /alias_and_pathinfo%s HTTP/1.0\r\n" % (PATH_INFO)
+        self.request          = "GET /%s%s HTTP/1.0\r\n" % (DIR, PATH_INFO)
         self.expected_error   = 200
         self.expected_content = "PATH_INFO = -%s-" % (PATH_INFO)
 
     def Prepare (self, www):
-        d = self.Mkdir (www, "alias_and_pathinfo")
+        d = self.Mkdir (www, TOP_DIR)
         f = self.WriteFile (d, "exec.cgi", 0755, CGI_BASE)
-        self.conf = CONF % (f)
+        self.conf = CONF % (DIR, f, d)
