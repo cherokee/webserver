4e5e9f6eda35087c614a17f4c6e5edbdba03244a
alobbs <alvaro@alobbs.com>
2009-03-17 20:52:45 +0000

    git-svn-id: svn://cherokee-project.com/cherokee/trunk@2984 5dc97367-97f1-0310-9951-d761b3857238

--- cherokee/connection.c
+++ cherokee/connection.c
@@ -716,12 +716,10 @@ cherokee_connection_build_header (cherokee_connection_t *conn)
 	 * has to be turned off.
 	 */
 	if (conn->keepalive != 0) {
-		if ((! http_type_300 (conn->error_code)) &&
-		    (! HANDLER_SUPPORTS (conn->handler, hsupport_length))) {
+		if (! HANDLER_SUPPORTS (conn->handler, hsupport_length)) {
 			if (! conn->chunked_encoding) {
 				conn->keepalive = 0;
 			}
-
 		} else {
 			conn->chunked_encoding = false;
 		}
