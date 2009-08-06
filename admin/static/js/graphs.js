35ff0d86cb82dabc086ffd0bc428d3c63fac2593
alobbs <alvaro@alobbs.com>
2009-08-06 09:34:13 +0000

    Tries to improve how the usage graphs are refreshed in the status page of cherokee-admin.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@3520 5dc97367-97f1-0310-9951-d761b3857238

--- admin/static/js/graphs.js
+++ admin/static/js/graphs.js
@@ -5,29 +5,36 @@ var graphInterval = '1h';
 var refInterval   = 60000;
 
 function graphChangeType(type, title) {
-    graphType = type;
     $('#gmtop').html("<span>" + title + "</span>");
-    refreshGraph();
+    graphType = type;
+    changeGraph(false);
 }
 
 function graphChangeInterval(i) {
     $("#g"+graphInterval).removeClass("gsel");
     $("#g"+i).addClass("gsel");
     graphInterval = i;
-    changeGraph();
+    changeGraph(true);
 }
 
-function changeGraph()
+function changeGraph(flash)
 {
     if (graphPrefix == 'vserver') {
 	   imgurl = '/graphs/' + graphPrefix + '_' + graphType + '_' + graphVServer + '_' + graphInterval + '.png';
     } else {
 	   imgurl = '/graphs/' + graphPrefix + '_' + graphType + '_' + graphInterval + '.png';
     }
-    $('#graphimg').fadeOut("slow").attr('src', imgurl).fadeIn("slow");
+
+    if (flash) {
+	   $('#graphimg').fadeOut("slow", function() {
+		  $('#graphimg').attr('src', imgurl).fadeIn("slow");
+	   });
+    } else {
+	   $('#graphimg').attr('src', imgurl);
+    }
 }
 
 function refreshGraph()
 {
-    setInterval(function(){ changeGraph(); }, refInterval);
+    setInterval(function(){ changeGraph(false); }, refInterval);
 }
