a6e0bc68469262b5ae85fea9bf7e2b62482e412a
alobbs <alvaro@alobbs.com>
2009-08-18 15:18:50 +0000

    Rewrites the QA so it stops checking whether the main QA application is running more than test execution thread. I have not been able to figure why I put that check in the first place; thus, it has been removed. This patch also removes a trailing slash at the end of the local directory path. It was causing the test to fail on the final test check.  Hopefully I'm not introducing a regression here..
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@3573 5dc97367-97f1-0310-9951-d761b3857238

--- qa/066-DirectoryIndex3.py
+++ qa/066-DirectoryIndex3.py
@@ -9,43 +9,34 @@ vserver!0660!rule!1!match = default
 vserver!0660!rule!1!handler = common
 """
 
-
 class Test (TestBase):
     def __init__ (self):
         TestBase.__init__ (self, __file__)
-        self.name = "Directory indexer, /index, vbles"
-
-        self.request           = "GET /inside/ HTTP/1.0\r\n" +\
-                                 "Host: directoryindex3\r\n"
-        self.expected_error    = 200
+        self.name           = "Directory indexer, /index, vbles"
+        self.request        = "GET /inside/ HTTP/1.0\r\n" +\
+                              "Host: directoryindex3\r\n"
+        self.expected_error = 200
 
     def Prepare (self, www):
-        self.dr = self.Mkdir (www, "directoryindex3/")
+        # Generate files and dir
+        self.dr = self.Mkdir (www, "directoryindex3")
         self.Mkdir (www, "directoryindex3/inside/foo")
 
-        self.expected_content  = ["DocumentRoot %s" % (self.dr),
-                                  "ScriptName /super_test_index.php",
-                                  "RequestUri /inside/"]
-
-        self.conf = CONF % (self.dr)
-
-        for php in self.php_conf.split("\n"):
-            self.conf += "vserver!0660!rule!%s\n" % (php)
-
-    def JustBefore (self, www):
         self.WriteFile (self.dr, "super_test_index.php", 0666, """<?php
                         echo "DocumentRoot ".$_SERVER[DOCUMENT_ROOT]."\n";
                         echo "ScriptName "  .$_SERVER[SCRIPT_NAME]."\n";
                         echo "RequestUri "  .$_SERVER[REQUEST_URI]."\n";
                         ?>""")
 
+        # Set the configuration
+        self.conf = CONF % (self.dr)
+        for php in self.php_conf.split("\n"):
+            self.conf += "vserver!0660!rule!%s\n" % (php)
+
+        # Expected output
+        self.expected_content = ["DocumentRoot %s" % (self.dr),
+                                 "ScriptName /super_test_index.php",
+                                 "RequestUri /inside/"]
+
     def Precondition (self):
-        if not os.path.exists (look_for_php()):
-            return False
-
-        f = os.popen("ps -p %d -L | wc -l" % (os.getpid()))
-        threads = int(f.read()[:-1])
-        f.close()
-
-        return (threads <= 2)
-        
+        return os.path.exists (look_for_php())
