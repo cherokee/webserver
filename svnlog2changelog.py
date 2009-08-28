60df1da7da8075c9cbd2815afb5f540c52132bd1
alobbs <alvaro@alobbs.com>
2009-08-28 07:46:12 +0000

    Just created an repository account for Roberto De Ioris.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@3596 5dc97367-97f1-0310-9951-d761b3857238

--- svnlog2changelog.py
+++ svnlog2changelog.py
@@ -12,10 +12,11 @@ from sys import stdin
 import xml.dom.minidom
 
 DEVELOPERS = {
-    'alo'    : "Alvaro Lopez Ortega  <alvaro@octality.com>",
-    'aperez' : "Antonio Perez  <aperez@skarcha.com>",
-    'ion'    : "Jonathan Hernandez  <ion@suavizado.com>",
-    'taher'  : "Taher Shihadeh  <taher@unixwars.com>"
+    'alo'          : "Alvaro Lopez Ortega  <alvaro@octality.com>",
+    'aperez'       : "Antonio Perez  <aperez@skarcha.com>",
+    'ion'          : "Jonathan Hernandez  <ion@suavizado.com>",
+    'taher'        : "Taher Shihadeh  <taher@unixwars.com>",
+    'robertounbit' : "Roberto De Ioris <roberto@unbit.it>"
 }
 
 def get_text (nodelist):
