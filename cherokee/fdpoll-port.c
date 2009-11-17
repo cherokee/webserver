7e86f052a99eb2b4e7fe381559ee49392e966aba
alobbs <alvaro@alobbs.com>
2009-11-17 06:07:21 +0000

    Fixes a compilation issue on OpenSolaris 2009.06 (with gcc). Thanks to Juan José Amor <jjamor@gmail.com> for the report.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@3835 5dc97367-97f1-0310-9951-d761b3857238

--- cherokee/fdpoll-port.c
+++ cherokee/fdpoll-port.c
@@ -202,8 +202,8 @@ _watch (cherokee_fdpoll_port_t *fdp, int timeout_msecs)
 		                   nfd, 
 		                   fdp->port_events[i].portev_user);
 		if ( rc < 0 ) {
-			LOG_ERRNO_S (errno, cherokee_err_error,
-				     CHEROKEE_ERROR_FDPOLL_PORTS_FD_ASSOCIATE, nfd);
+			LOG_ERRNO (errno, cherokee_err_error,
+				   CHEROKEE_ERROR_FDPOLL_PORTS_FD_ASSOCIATE, nfd);
 		}
 	}
 
