149d481d56f30832d54af771ebef2d120bfaf633
alobbs <alvaro@alobbs.com>
2009-12-18 10:04:59 +0000

    Updates the developers list.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@4032 5dc97367-97f1-0310-9951-d761b3857238

--- developers.py
+++ developers.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+
 DEVELOPERS = {
     'alo'          : "Alvaro Lopez Ortega  <alvaro@octality.com>",
     'aperez'       : "Antonio Perez  <aperez@skarcha.com>",
     'ion'          : "Jonathan Hernandez  <ion@suavizado.com>",
     'taher'        : "Taher Shihadeh  <taher@unixwars.com>",
-    'robertounbit' : "Roberto De Ioris <roberto@unbit.it>"
+    'robertounbit' : "Roberto De Ioris <roberto@unbit.it>",
+    'gefire'       : "李炅 <lijiong1986@126.com>",
+    'skinkie'      : "Stefan de Konink <stefan@konink.de>"
 }
