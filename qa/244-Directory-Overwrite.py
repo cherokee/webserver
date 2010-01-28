cherokee/webserver

commit bedf48fa6bd7ea062c517eb762e984c55ff8cfcf

Fixes an issue with PATH_INFO when a directory rule didn't have a handler assigned.
git-svn-id: svn://cherokee-project.com/cherokee/trunk@4178 5dc97367-97f1-0310-9951-d761b3857238

cherokee/rule_default.c
@@ -39,10 +39,13 @@ match (cherokee_rule_t         *rule,
        cherokee_connection_t   *conn,
        cherokee_config_entry_t *ret_conf)
 {
-	UNUSED(rule);
 	UNUSED(ret_conf);
 
-	if (cherokee_buffer_is_empty (&conn->web_directory)) {
+	if (rule->config.handler_new_func != NULL) {
+		cherokee_buffer_clean (&conn->web_directory);
+		cherokee_buffer_add_str (&conn->web_directory, "/");
+
+	} else if (cherokee_buffer_is_empty (&conn->web_directory)) {
 		cherokee_buffer_add_str (&conn->web_directory, "/");
 	}
 

cherokee/rule_directory.c
@@ -66,11 +66,6 @@ match (cherokee_rule_directory_t *rule,
 		return ret_not_found;
 	}
 
-	/* Copy the web directory property
-	 */
-	cherokee_buffer_clean      (&conn->web_directory);
-	cherokee_buffer_add_buffer (&conn->web_directory, &rule->directory);
-
 	/* If the request is exactly the directory entry, and it
 	 * doesn't end with a slash, it must be redirected. Eg:
 	 *
@@ -92,10 +87,11 @@ match (cherokee_rule_directory_t *rule,
 		return ret_error;
 	}
 
-	/* Set the web_directory if needed
+	/* Copy the web directory property
 	 */
-	if (cherokee_buffer_is_empty (&conn->web_directory)) {
-		cherokee_buffer_add_str (&conn->web_directory, "/");
+	if (RULE(rule)->config.handler_new_func != NULL) {
+		cherokee_buffer_clean      (&conn->web_directory);
+		cherokee_buffer_add_buffer (&conn->web_directory, &rule->directory);
 	}
 
 	TRACE(ENTRIES, "Match! rule=%s req=%s web_directory=%s: ret_ok\n",

commit 0016d479df02a558476b66c8fea4a3788cf5099f

Fixes a regression introduced in r4178. Also adds a QA test for this patch. Bug: http://bugs.cherokee-project.com/724
git-svn-id: svn://cherokee-project.com/cherokee/trunk@4190 5dc97367-97f1-0310-9951-d761b3857238

cherokee/rule_default.c
@@ -39,13 +39,10 @@ match (cherokee_rule_t         *rule,
        cherokee_connection_t   *conn,
        cherokee_config_entry_t *ret_conf)
 {
+	UNUSED(rule);
 	UNUSED(ret_conf);
 
-	if (rule->config.handler_new_func != NULL) {
-		cherokee_buffer_clean (&conn->web_directory);
-		cherokee_buffer_add_str (&conn->web_directory, "/");
-
-	} else if (cherokee_buffer_is_empty (&conn->web_directory)) {
+	if (cherokee_buffer_is_empty (&conn->web_directory)) {
 		cherokee_buffer_add_str (&conn->web_directory, "/");
 	}
 

cherokee/rule_directory.c
@@ -89,7 +89,9 @@ match (cherokee_rule_directory_t *rule,
 
 	/* Copy the web directory property
 	 */
-	if (RULE(rule)->config.handler_new_func != NULL) {
+	if ((RULE(rule)->config.handler_new_func != NULL) ||
+	    (RULE(rule)->config.document_root != NULL))
+	{
 		cherokee_buffer_clean      (&conn->web_directory);
 		cherokee_buffer_add_buffer (&conn->web_directory, &rule->directory);
 	}

qa/244-Directory-Overwrite.py
@@ -1,0 +1,39 @@
+import os
+from base import *
+
+MAGIC = "Cherokee Rocks!"
+
+CONF = """
+vserver!2440!nick = directoryoverwrite
+vserver!2440!document_root = %s
+vserver!2440!directory_index = index.html
+vserver!2440!rule!300!document_root = %s
+vserver!2440!rule!300!match = directory
+vserver!2440!rule!300!match!directory = /inside
+vserver!2440!rule!300!match!final = 0
+vserver!2440!rule!100!handler = common
+vserver!2440!rule!100!handler!iocache = 0
+vserver!2440!rule!100!match = default
+vserver!2440!rule!100!match!final = 1
+"""
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self, __file__)
+        self.name           = "Rule: Directory properties overwrite"
+        self.request        = "GET /inside/ HTTP/1.0\r\n" +\
+                              "Host: directoryoverwrite\r\n"
+        self.expected_error = 200
+
+    def Prepare (self, www):
+        # Generate files and dir
+        self.vsdr = self.Mkdir (www, "directoryoverwrite")
+        self.rldr = self.Mkdir (www, "directoryinside")
+
+        self.WriteFile (self.rldr, "index.html", 0666, MAGIC)
+
+        # Set the configuration
+        self.conf = CONF % (self.vsdr, self.rldr)
+
+        # Expected output
+        self.expected_content = [MAGIC]