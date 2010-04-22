572cdf769a3a3155ff592b54a3e34159a3d8de29
taher <taher@shihadeh.net>
2010-04-22 13:56:45 +0000

    Replaces wrong title
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@4919 5dc97367-97f1-0310-9951-d761b3857238

--- admin/plugins/redir.py
+++ admin/plugins/redir.py
@@ -113,7 +113,7 @@ class Plugin_redir (Handler.PluginHandler):
         submit += CTK.SubmitterButton(_('Add'))
         submit.bind ('submit_success', refresh.JS_to_refresh())
 
-        self += CTK.RawHTML ("<h2>%s</h2>" % (_('Add New Rule')))
+        self += CTK.RawHTML ("<h2>%s</h2>" % (_('Add New Regular Expression')))
         self += CTK.Indenter (submit)
 
 
