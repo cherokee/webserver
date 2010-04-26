7ab4d556581c6630744776647d1d246f66e5f436
alobbs <alvaro@alobbs.com>
2010-04-26 11:13:30 +0000

    Final keys were not being handled properly. It fixes the bug #820: http://bugs.cherokee-project.com/820
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@4957 5dc97367-97f1-0310-9951-d761b3857238

--- admin/PageRule.py
+++ admin/PageRule.py
@@ -226,14 +226,14 @@ class Render:
                                    "$(this).parents('.row_content').toggleClass('rule-inactive');")
 
                     # Final
-                    is_final = bool (int (CTK.cfg.get_val('vserver!%s!rule!%s!final'%(vsrv_num,r), "1")))
+                    is_final = bool (int (CTK.cfg.get_val('vserver!%s!rule!%s!match!final'%(vsrv_num,r), "1")))
 
                     final = CTK.ToggleButtonImages (CTK.Box({'class': 'final-on'},  CTK.RawHTML(_('Final'))),
                                                     CTK.Box({'class': 'final-off'}, CTK.RawHTML(_('Non Final'))),
                                                     is_final)
                     final.bind ('changed',
                                 CTK.JS.Ajax (url_apply, async=True,
-                                             data = '{"vserver!%s!rule!%s!final": parseInt(event.value)?"0":"1"}'%(vsrv_num,r)))
+                                             data = '{"vserver!%s!rule!%s!match!final": parseInt(event.value)?"0":"1"}'%(vsrv_num,r)))
 
                     # Actions
                     group = CTK.Box ({'class': 'sel-actions'}, [disabled, remove])
