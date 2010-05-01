6bce9dee4c14929b4c679c06853101848794d888
alobbs <alvaro@alobbs.com>
2010-05-01 18:17:01 +0000

    Adds a new script to estimate the state of the different translations.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@5006 5dc97367-97f1-0310-9951-d761b3857238

---
+++ po/po_stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Cherokee PO stats
+#
+# Authors:
+#      Alvaro Lopez Ortega <alvaro@alobbs.com>
+#
+# Copyright (C) 2010 Alvaro Lopez Ortega
+# This file is distributed under the GPL2 license.
+
+import os
+import re
+import sys
+
+# Check parameters
+if len(sys.argv) < 2:
+    print "ERROR: %s admin/*.po" %(sys.argv[0])
+    raise SystemExit
+
+files = sys.argv[1:]
+
+# Process the PO files
+po_list  = []
+file_max = max([len(x) for x in files])
+
+for file in files:
+    content = open(file, 'r').read()
+    pad = ' ' * (file_max - len(file))
+
+    unfinished = len (re.findall(r'msgstr ""\n\n', content, re.M))
+    total      = len (re.findall(r'msgid', content, re.M))
+    finished   = total - unfinished
+
+    percent_unfin = (100 * unfinished) / float(total)
+    percent_fin   = 100 - percent_unfin
+
+    po_list.append ({'file':        file,
+                     'percent_fin': percent_fin,
+                     'finished':    finished,
+                     'total':       total})
+
+# Print the results
+po_list.sort (lambda x,y: cmp(x['percent_fin'], y['percent_fin']))
+po_list.reverse()
+
+for e in po_list:
+    print "%(file)s\t%(percent_fin) 3d%%\t%(finished)d/%(total)d" %(e)
