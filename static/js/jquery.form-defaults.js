f54c3b3300eeb2b320c009628336e14d0d3518cd
alobbs <alvaro@alobbs.com>
2010-05-03 10:58:50 +0000

    Optional CTK.TextField() widgets add an 'optional_msg' class whenever they show the default message (instead of a real value). It'll allow other widgets to easily recognize whether or not text fields contain useful values.
    git-svn-id: svn://cherokee-project.com/CTK/trunk@5014 5dc97367-97f1-0310-9951-d761b3857238

--- static/js/jquery.form-defaults.js
+++ static/js/jquery.form-defaults.js
@@ -8,8 +8,10 @@ jQuery.fn.DefaultValue = function(klass, text){
 	   if(val == text || val == '') {
 		  self.val (text);
 		  self.addClass (klass);
+		  self.addClass ('optional_msg');
 	   } else {
 		  self.removeClass (klass);
+		  self.removeClass ('optional_msg');
 	   }
 
     }
@@ -17,31 +19,33 @@ jQuery.fn.DefaultValue = function(klass, text){
     return this.each(function(){
 	   var self = $(this);
 
-	   //Make sure we're dealing with text-based form fields
+	   // Make sure we're dealing with text-based form fields
 	   if (this.type != 'text' && this.type != 'password' && this.type != 'textarea') {
 		  return;
 	   }
 
-	   //Set value initially if none are specified
+	   // Set value initially if none are specified
 	   update (self);
 
-	   //Remove values on focus
+	   // Remove values on focus
 	   self.focus(function() {
 		  if(self.val() == text || self.val() == '') {
-			 self.removeClass(klass);
+			 self.removeClass (klass);
+			 self.removeClass ('optional_msg');
 			 self.val('');
 		  }
 	   });
 
-	   //Place values back on blur
+	   // Place values back on blur
 	   self.blur(function() {
 		  if(self.val() == text || self.val() == '') {
-			 self.addClass(klass);
+			 self.addClass (klass);
+			 self.addClass ('optional_msg');
 			 self.val(text);
 		  }
 	   });
 
-	   //Update
+	   // Update
 	   self.bind('update', function() {
 		  update (self);
 	   });
