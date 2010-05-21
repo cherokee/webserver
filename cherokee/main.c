cherokee/webserver

commit faa047bbe64813b1109e46e8d9125044a0dcbdf9

Always clean up on exit. It should fix http://bugs.cherokee-project.com/525
git-svn-id: svn://cherokee-project.com/cherokee/trunk@3435 5dc97367-97f1-0310-9951-d761b3857238

cherokee/main.c
@@ -852,9 +852,6 @@ main (int argc, char *argv[])
 			DELAY_ERROR);
 	}
 
-	if (! single_time) {
-		clean_up();
-	}
-
+	clean_up();
 	return EXIT_OK;
 }

commit 46ffef770f4b9234eb566d5ee024a7beb5bc3bd3

Fixes a regression introduced in r3435. It should not clean up the PID file if cherokee is executed in "single time" mode.
git-svn-id: svn://cherokee-project.com/cherokee/trunk@5142 5dc97367-97f1-0310-9951-d761b3857238

cherokee/main.c
@@ -939,6 +939,9 @@ main (int argc, char *argv[])
 			DELAY_ERROR);
 	}
 
-	clean_up();
+	if (! single_time) {
+		clean_up();
+	}
+
 	return EXIT_OK;
 }