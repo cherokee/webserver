86bc0e7b2bd9df07bf6e245d6e792b573ed7ef6a
alobbs <alvaro@alobbs.com>
2010-06-13 12:26:51 +0000

    Improves the new CTK.Downloader widget.
    git-svn-id: svn://cherokee-project.com/CTK/trunk@5195 5dc97367-97f1-0310-9951-d761b3857238

--- CTK/Downloader.py
+++ CTK/Downloader.py
@@ -29,14 +29,14 @@ from threading import Thread
 import JS
 from Box import Box
 from ProgressBar import ProgressBar
-from Server import publish
+from Server import publish, request
 
 JS_UPDATING = """
 function update_progress_%(id)s() {
   /* Retrieve status
    */
   info = {};
-  $.ajax ({type: 'GET', url: "%(url)s", async: false, dataType: "json",
+  $.ajax ({type: 'GET', url: "%(url)s/info", async: false, dataType: "json",
            success: function (data) {
                for (i in data) {
                  info[i] = data[i];
@@ -59,10 +59,21 @@ function update_progress_%(id)s() {
 
   } else if (info.status == 'finished') {
         $('#%(progressbar_id)s').trigger ({'type':'finished'});
+
+  } else if (info.status == 'error') {
+        $('#%(progressbar_id)s').trigger ({'type':'error'});
   }
 }
-window.setTimeout (update_progress_%(id)s, 1000);
 """
+
+LAUNCH_DOWNLOAD_JS = """
+$.ajax ({type: 'GET', url: "%(url)s/start", async: false,
+   success: function (data) {
+      window.setTimeout (update_progress_%(id)s, 1000);
+   }
+});
+"""
+
 
 # Current downloads
 downloads = {}
@@ -83,30 +94,44 @@ class DownloadEntry (Thread):
         self.request = urllib2.Request (self.url)
 
         # HTTP Request
-        self.response = self.opener.open (self.request)
+        try:
+            self.response = self.opener.open (self.request)
+        except Exception, e:
+            print e
+            self.status = 'error'
+
         self.size = int(self.response.headers.getheaders("Content-Length")[0])
         self.downloaded = 0
 
         self.status      = 'downloading'
         self.time_start  = time.time()
-        self.target_temp = tempfile.TemporaryFile()
+
+        # Temporal storage
+        fd, path = tempfile.mkstemp()
+        self.target_temp = os.fdopen (fd, "w+")
+        self.target_path = path
 
         # Read response
         while True:
             # I/O
-            chunk = self.response.read(1024)
-            print "read len %d" %(len(chunk))
-            if not chunk:
+            try:
+                chunk = self.response.read(1024)
+                if not chunk:
+                    # Download finished
+                    self.status = 'finished'
+                    break
+                self.target_temp.write (chunk)
+            except Exception, e:
+                print e
+                # Error
+                self.status = 'error'
                 break
-            self.target_temp.write (chunk)
 
             # Stats
             self.downloaded += len(chunk)
             self.percent = int(self.downloaded * 100 / self.size)
             print "Downloading thread", "%d%%" %(self.percent)
 
-        # Download finished
-        self.status = 'finished'
 
 
 def DownloadEntry_Factory (url, *args, **kwargs):
@@ -115,17 +140,21 @@ def DownloadEntry_Factory (url, *args, **kwargs):
     if not url in downloads:
         tmp = DownloadEntry (url, *args, **kwargs)
         downloads[url] = tmp
-        tmp.start()
     return downloads[url]
 
 
 class DownloadReport:
    def __call__ (self, url):
-       d = downloads[url]
+       if request.url.endswith('/info'):
+           d = downloads[url]
+           return '{"status": "%s", "percent": %d, "size": %d, "downloaded": %d}' %(
+               d.status, d.percent, d.size, d.downloaded)
 
-       return '{"status": "%s", "percent": %d, "size": %d, "downloaded": %d}' %(
-           d.status, d.percent, d.size, d.downloaded)
-
+       elif request.url.endswith('/start'):
+           # Current Downloads
+           d = downloads[url]
+           d.start()
+           return 'ok'
 
 class Downloader (Box):
     def __init__ (self, url, props={}):
@@ -133,17 +162,15 @@ class Downloader (Box):
         self.url = url
         self.id  = "downloader_%d" %(self.uniq_id)
 
-        # Current Downloads
-        download = DownloadEntry_Factory (self.url)
+        # Other GUI components
+        self.progressbar = ProgressBar()
+        self += self.progressbar
 
         # Register the uploader path
         self._url_local = "/downloader_%d_report" %(self.uniq_id)
         publish (self._url_local, DownloadReport, url=url)
 
-        # Other GUI components
-        self.progressbar = ProgressBar()
-        self += self.progressbar
-
+        download = DownloadEntry_Factory (self.url)
 
     def Render (self):
         render = Box.Render(self)
@@ -155,4 +182,9 @@ class Downloader (Box):
         render.js += JS_UPDATING %(props)
         return render
 
+    def JS_to_start (self):
+        props = {'id':  self.id,
+                 'url': self._url_local}
+
+        return LAUNCH_DOWNLOAD_JS %(props)
 
