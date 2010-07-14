5e123542015f2abd4f963475d343c8dfe56e7038
alobbs <alvaro@alobbs.com>
2010-07-14 11:37:25 +0000

    Fixes an issue in the dirlist handler. It was failing to list some entries when the target directory was a symbolic link.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@5313 5dc97367-97f1-0310-9951-d761b3857238

--- cherokee/handler_dirlist.c
+++ cherokee/handler_dirlist.c
@@ -329,7 +329,29 @@ is_file_in_list (cherokee_list_t *list, char *filename, cuint_t len)
 
 
 static ret_t
-generate_file_entry (cherokee_handler_dirlist_t *dhdl, DIR *dir, cherokee_buffer_t *path, file_entry_t **ret_entry)
+realpath_buf (cherokee_buffer_t *in,
+	      cherokee_buffer_t *resolved)
+{
+	char *re;
+
+	cherokee_buffer_ensure_size (resolved, PATH_MAX);
+
+	re = realpath (in->buf, resolved->buf);
+	if (re == NULL) {
+		return ret_error;
+	}
+
+	resolved->len = strlen (resolved->buf);
+	return ret_ok;
+}
+
+
+static ret_t
+generate_file_entry (cherokee_handler_dirlist_t  *dhdl,
+		     DIR                         *dir,
+		     cherokee_buffer_t           *path,
+		     cherokee_buffer_t           *local_realpath,
+		     file_entry_t               **ret_entry)
 {
 	int            re;
 	ret_t          ret;
@@ -402,11 +424,25 @@ generate_file_entry (cherokee_handler_dirlist_t *dhdl, DIR *dir, cherokee_buffer
 			if (HDL_DIRLIST_PROP(dhdl)->redir_symlinks) {
 				char *res;
 
-				/* Read the real path
+				/* The local directory realpath is build lazily
 				 */
-				cherokee_buffer_ensure_size (&n->realpath, PATH_MAX);
-				res = realpath (path->buf, n->realpath.buf);
-				if (res == NULL) {
+				if (cherokee_buffer_is_empty (local_realpath))
+				{
+					cherokee_buffer_drop_ending (path, n->name_len);
+
+					ret = realpath_buf (path, local_realpath);
+					if (unlikely (ret != ret_ok)) {
+						file_entry_free (n);
+						return ret_error;
+					}
+
+					cherokee_buffer_add (path, name, n->name_len);
+				}
+
+				/* Read the real path of the entry
+				 */
+				ret = realpath_buf (path, &n->realpath);
+				if (unlikely (ret != ret_ok)) {
 					cherokee_buffer_drop_ending (path, n->name_len);
 
 					file_entry_free (n);
@@ -415,11 +451,9 @@ generate_file_entry (cherokee_handler_dirlist_t *dhdl, DIR *dir, cherokee_buffer
 
 				/* Check it is within the limits
 				 */
-				n->realpath.len = strlen (n->realpath.buf);
-				re = strncmp (n->realpath.buf, path->buf,
-					      path->len - n->name_len);
-
+				re = strncmp (n->realpath.buf, local_realpath->buf, local_realpath->len);
 				if (re != 0) {
+					cherokee_buffer_drop_ending (path, n->name_len);
 					file_entry_free (n);
 					return ret_error;
 				}
@@ -658,13 +692,14 @@ build_file_list (cherokee_handler_dirlist_t *dhdl)
 	file_entry_t          *item;
 	int                    is_dir;
 	int                    is_link;
-	cherokee_connection_t *conn    = HANDLER_CONN(dhdl);
+	cherokee_connection_t *conn           = HANDLER_CONN(dhdl);
+	cherokee_buffer_t      local_realpath = CHEROKEE_BUF_INIT;
 
 	/* Build the local directory path
 	 */
 	cherokee_buffer_add_buffer (&conn->local_directory, &conn->request);        /* 1 */
-	dir = opendir (conn->local_directory.buf);
 
+	dir = opendir (conn->local_directory.buf);
 	if (dir == NULL) {
 		conn->error_code = http_not_found;
 		return ret_error;
@@ -675,7 +710,7 @@ build_file_list (cherokee_handler_dirlist_t *dhdl)
 	for (;;) {
 		ret_t ret;
 
-		ret = generate_file_entry (dhdl, dir, &conn->local_directory, &item);
+		ret = generate_file_entry (dhdl, dir, &conn->local_directory, &local_realpath, &item);
 		if (ret == ret_eof)
 			break;
 		if ((ret == ret_nomem) ||
@@ -700,6 +735,11 @@ build_file_list (cherokee_handler_dirlist_t *dhdl)
 	 */
 	closedir(dir);
 	cherokee_buffer_drop_ending (&conn->local_directory, conn->request.len); /* 2 */
+
+	/* Free local_realpath. It might have been built lazily,
+	 * inside the generate_file_entry() function.
+	 */
+	cherokee_buffer_mrproper (&local_realpath);
 
 	/* Sort the file list
 	 */
