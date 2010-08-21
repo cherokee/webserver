196c9673db5190843506b782a954187573e8d3bc
alobbs <alvaro@alobbs.com>
2010-08-21 17:30:05 +0000

    Better examples for CTK.StarRating
    git-svn-id: svn://cherokee-project.com/CTK/trunk@5416 5dc97367-97f1-0310-9951-d761b3857238

--- tests/test14.py
+++ tests/test14.py
@@ -8,12 +8,18 @@ def commit():
 
 def default():
     submit = CTK.Submitter('/commit')
+
     submit += CTK.RawHTML ("<h2>Can set, without initial value</h2>")
-    submit += CTK.StarRating ({'name': 'test_rate2', 'can_set': True})
+    submit += CTK.StarRating ({'name': 'test_rate1', 'can_set': True})
+
     submit += CTK.RawHTML ("<h2>Can set, with initial value</h2>")
-    submit += CTK.StarRating ({'name': 'test_rate1', 'selected': '3', 'can_set': True})
+    submit += CTK.StarRating ({'name': 'test_rate2', 'selected': '3', 'can_set': True})
+
     submit += CTK.RawHTML ("<h2>Cannot edit value</h2>")
-    submit += CTK.StarRating ({'name': 'test_rate2', 'selected': '4'})
+    submit += CTK.StarRating ({'name': 'test_rate3', 'selected': '4'})
+
+    submit += CTK.RawHTML ("<h2>No auto-submit</h2>")
+    submit += CTK.StarRating ({'name': 'test_rate4', 'can_set': True, 'class': 'noauto'})
 
     page = CTK.Page()
     page += CTK.RawHTML('<h1>Demo StarRating</h1>')
