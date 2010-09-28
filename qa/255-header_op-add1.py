b948d367b4cdf99fadd77fc565735a01a407b2f0
alobbs <alvaro@alobbs.com>
2010-09-28 00:11:33 +0000

    Adds two new QA tests to cover the new header operations parameters.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@5567 5dc97367-97f1-0310-9951-d761b3857238

---
+++ qa/255-header_op-add1.py
@@ -0,0 +1,31 @@
+from base import *
+
+DIR    = "headerop_add1"
+HEADER = "lalelilolu"
+VALUE  = "barfoo"
+
+CONF = """
+vserver!1!rule!2550!match = directory
+vserver!1!rule!2550!match!directory = /%(DIR)s
+vserver!1!rule!2550!handler = dirlist
+vserver!1!rule!2550!header_op!1!type = add
+vserver!1!rule!2550!header_op!1!header = %(HEADER)s
+vserver!1!rule!2550!header_op!1!value = %(VALUE)s
+"""
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self, __file__)
+        self.name           = "Header Ops: Add header"
+        self.request        = "GET /%s/ HTTP/1.0\r\n" %(DIR)
+        self.expected_error = 200
+        self.conf           = CONF%(globals())
+
+    def CustomTest (self):
+        header = self.reply[:self.reply.find("\r\n\r\n")]
+        if not "%(HEADER)s: %(VALUE)s\r\n" %(globals()) in header:
+            return -1
+        return 0
+
+    def Prepare (self, www):
+        self.Mkdir (www, DIR)
---
+++ qa/256-header_op-add2.py
@@ -0,0 +1,40 @@
+from base import *
+
+DIR = "headerop_add2"
+
+HEADERS = [("X-What-Rocks", "Cherokee does"),
+           ("X-What-Sucks", "Failed QA tests"),
+           ("X-What-Is-It", "A successful test")]
+
+CONF = """
+vserver!1!rule!2560!match = directory
+vserver!1!rule!2560!match!directory = /%(DIR)s
+vserver!1!rule!2560!handler = dirlist
+"""
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self, __file__)
+        self.name           = "Header Ops: Add multiple headers"
+        self.request        = "GET /%s/ HTTP/1.0\r\n" %(DIR)
+        self.expected_error = 200
+        self.conf           = CONF%(globals())
+
+        n = 2
+        for h,v in HEADERS:
+            self.conf += "vserver!1!rule!2560!header_op!%d!type = add\n"  %(n)
+            self.conf += "vserver!1!rule!2560!header_op!%d!header = %s\n" %(n, h)
+            self.conf += "vserver!1!rule!2560!header_op!%d!value = %s\n"  %(n, v)
+            n += 1
+
+    def CustomTest (self):
+        header = self.reply[:self.reply.find("\r\n\r\n")]
+
+        for h,v in HEADERS:
+            if not "%s: %s\r\n" %(h,v) in header:
+                return -1
+
+        return 0
+
+    def Prepare (self, www):
+        self.Mkdir (www, DIR)
