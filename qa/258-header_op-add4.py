15974c3cb5389efd8aa9c82e672d2ac8114e67d4
alobbs <alvaro@alobbs.com>
2010-09-28 13:53:05 +0000

    Adds a new QA test to check sequential header updates. In this case, adds a remove a header entry, after which checks that the header is not present in the response header.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@5576 5dc97367-97f1-0310-9951-d761b3857238

---
+++ qa/258-header_op-add4.py
@@ -0,0 +1,38 @@
+import re
+from base import *
+
+DIR    = "headerop_add4"
+HEADER = "X-header-check"
+VALUE  = "1234567890ABCD"
+
+CONF = """
+vserver!1!rule!2580!match = directory
+vserver!1!rule!2580!match!directory = /%(DIR)s
+vserver!1!rule!2580!handler = dirlist
+vserver!1!rule!2580!header_op!1!type = add
+vserver!1!rule!2580!header_op!1!header = %(HEADER)s
+vserver!1!rule!2580!header_op!1!value = %(VALUE)s
+vserver!1!rule!2580!header_op!2!type = del
+vserver!1!rule!2580!header_op!2!header = %(HEADER)s
+"""
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self, __file__)
+        self.name           = "Header Ops: Add header, multiple"
+        self.request        = "GET /%s/ HTTP/1.0\r\n" %(DIR)
+        self.expected_error = 200
+        self.conf           = CONF%(globals())
+
+    def CustomTest (self):
+        header = self.reply[:self.reply.find("\r\n\r\n")]
+
+        if HEADER in header:
+            return -1
+        if VALUE in header:
+            return -1
+
+        return 0
+
+    def Prepare (self, www):
+        self.Mkdir (www, DIR)
