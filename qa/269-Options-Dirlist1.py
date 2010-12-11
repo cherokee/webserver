1afdb39b8492a13f54ddf1735e24b5dd4e6d175a
alobbs <alvaro@alobbs.com>
2010-12-11 17:34:34 +0000

    Adds a new QA test to check whether the 'dirlist' handler includes an 'Allow:' header when replying a OPTIONS request.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@5919 5dc97367-97f1-0310-9951-d761b3857238

---
+++ qa/269-Options-Dirlist1.py
@@ -0,0 +1,14 @@
+from base import *
+
+DIR = "269_options_dirlist"
+
+class Test (TestBase):
+    def __init__ (self):
+        TestBase.__init__ (self, __file__)
+        self.name              = "OPTIONS: Dirlist request"
+        self.request           = "OPTIONS /%(DIR)s/ HTTP/1.0\r\n" % (globals())
+        self.expected_error    = 200
+        self.expected_content  = ["Allow: ", "GET", "OPTIONS", ", "]
+
+    def Prepare (self, www):
+        d = self.Mkdir (www, DIR)
