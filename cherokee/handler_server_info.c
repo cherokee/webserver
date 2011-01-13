d4d062b5bb48977796f58059982d1c250edc1b84
alobbs <alvaro@alobbs.com>
2011-01-13 09:15:33 +0000

    Adds two new entries to the 'Server Info' handler external data structure: "accepts" and "timeouts". Thanks Gavin M. Roy for the patch! Implements RFE #1111: http://bugs.cherokee-project.com/1111
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@6094 5dc97367-97f1-0310-9951-d761b3857238

--- cherokee/handler_server_info.c
+++ cherokee/handler_server_info.c
@@ -386,7 +386,15 @@ add_traffic (cherokee_dwriter_t *writer,
 		cherokee_dwriter_cstring (writer, "unknown");
 	}
 
+	cherokee_dwriter_dict_open (writer);
 	cherokee_dwriter_cstring (writer, "accepts");
+	if (srv->collector) {
+		cherokee_dwriter_integer (writer, COLLECTOR(srv->collector)->accepts);
+	} else {
+		cherokee_dwriter_number (writer, "-1", 2);
+	}
+
+	cherokee_dwriter_cstring (writer, "accepts_formatted");
 	if (srv->collector != NULL) {
 		cherokee_buffer_clean     (&tmp);
 		cherokee_buffer_add_fsize (&tmp, COLLECTOR(srv->collector)->accepts);
@@ -395,7 +403,15 @@ add_traffic (cherokee_dwriter_t *writer,
 		cherokee_dwriter_cstring (writer, "unknown");
 	}
 
+	cherokee_dwriter_dict_open (writer);
 	cherokee_dwriter_cstring (writer, "timeouts");
+	if (srv->collector) {
+		cherokee_dwriter_integer (writer, COLLECTOR(srv->collector)->timeouts);
+	} else {
+		cherokee_dwriter_number (writer, "-1", 2);
+	}
+
+	cherokee_dwriter_cstring (writer, "timeouts_formatted");
 	if (srv->collector != NULL) {
 		cherokee_buffer_clean     (&tmp);
 		cherokee_buffer_add_fsize (&tmp, COLLECTOR(srv->collector)->timeouts);
