4a6d75be8c80a62f9f81e1eb6f3e5a48c9438b74
taher <taher@shihadeh.net>
2011-02-09 09:13:05 +0000

    Enhanced market installation detection
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@6263 5dc97367-97f1-0310-9951-d761b3857238

--- admin/PageVServers.py
+++ admin/PageVServers.py
@@ -189,7 +189,9 @@ class Render:
             for k in vservers:
                 # Document root widget
                 droot_str = CTK.cfg.get_val ('vserver!%s!document_root'%(k), '')
-                if droot_str.startswith (CHEROKEE_OWS_ROOT):
+
+                # Market install base scenario
+                if self.__is_market_installation (k, droot_str):
                     droot_widget = CTK.Box ({'class': 'droot'}, CTK.RawHTML(_('Market installation')))
                 else:
                     droot_widget = CTK.Box ({'class': 'droot'}, CTK.RawHTML(CTK.escape_html (droot_str)))
@@ -232,6 +234,24 @@ class Render:
 
                     # List entry
                     panel.Add (k, '/vserver/content/%s'%(k), content, True, disclass)
+
+
+        def __is_market_installation (self, k, droot_str):
+            # Market install base scenario
+            if droot_str.startswith (CHEROKEE_OWS_ROOT):
+                return True
+
+            # Market install with no droot
+            elif droot_str == '/dev/null':
+                rules = CTK.cfg['vserver!%s!rule'%(k)].keys()
+                for rule_num in rules:
+                    src_num = CTK.cfg.get_val ('vserver!%s!rule!%s!handler!balancer!source!1' %(k,rule_num), '')
+                    src_int = CTK.cfg.get_val ('source!%s!interpreter'%(src_num), '')
+                    if CHEROKEE_OWS_ROOT in src_int:
+                        return True
+
+            return False
+
 
     class PanelButtons (CTK.Box):
         def __init__ (self):
