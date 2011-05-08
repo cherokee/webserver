eb0e19fb10380eb420e16dd365a0ee817503770e
alobbs <alvaro@alobbs.com>
2011-05-08 16:09:34 +0000

    Enhances the Front-Line Cache wizard to support the set-up of the special PURGE method.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@6599 5dc97367-97f1-0310-9951-d761b3857238

--- admin/wizards/flcache.py
+++ admin/wizards/flcache.py
@@ -32,8 +32,11 @@ NOTE_WELCOME_H1 = N_("Welcome to the Cache wizard")
 NOTE_WELCOME_P1 = N_("This wizard adds a rule to configure the cache mechanism shipped with Cherokee.")
 NOTE_WELCOME_P2 = N_("It will boost the performance of your virtual server by caching content, and thus optimizing subsequent requests of the same Web resource.")
 
-NOTE_POLICY   = N_("What do you want the server to cache?")
-NOTE_ENC_EXTS = N_("Which extensions you want the Cache to store?")
+NOTE_POLICY         = N_("What do you want the server to cache?")
+NOTE_AUTH           = N_("Enable HTTP's PURGE method, so cache object can be purged remotely.")
+NOTE_ENC_EXTS       = N_("Which extensions you want the Cache to store?")
+NOTE_ADMIN_USER     = N_("Administration user name")
+NOTE_ADMIN_PASSWORD = N_("Administration user's password")
 
 PREFIX    = 'tmp!wizard!flcache'
 URL_APPLY = r'/wizard/vserver/flcache/apply'
@@ -59,7 +62,7 @@ ENCODED_EXTS_CONF    = """
 %(rule_pre)s!encoder!gzip = allow
 """
 
-GLOBAL_CACHE_CONF    = """
+GLOBAL_CACHE_CONF = """
 %(rule_pre)s!match!final = 0
 %(rule_pre)s!match = directory
 %(rule_pre)s!match!directory = /
@@ -67,10 +70,23 @@ GLOBAL_CACHE_CONF    = """
 %(rule_pre)s!flcache!policy = explicitly_allowed
 """
 
+PURGE_METHOD_CONF = """
+%(rule_pre)s!match!final = 1
+%(rule_pre)s!match = method
+%(rule_pre)s!match!method = purge
+%(rule_pre)s!handler = admin
+%(rule_pre)s!auth = authlist
+%(rule_pre)s!auth!methods = digest
+%(rule_pre)s!auth!realm = Front-Line Cache
+%(rule_pre)s!auth!list!1!password = %(password)s
+%(rule_pre)s!auth!list!1!user = %(username)s
+"""
+
 class Commit:
     def Commit_Rule (self):
         vsrv_num = CTK.cfg.get_val ('%s!vsrv_num'%(PREFIX))
         tipe     = CTK.cfg.get_val ('%s!policy'  %(PREFIX))
+        purge    = int (CTK.cfg.get_val('%s!purge'%(PREFIX), "0"))
         vsrv_pre = 'vserver!%s' %(vsrv_num)
 
         # Next rule
@@ -88,6 +104,18 @@ class Commit:
         CTK.cfg.apply_chunk (config)
         CTK.cfg.normalize ('%s!rule'%(vsrv_pre))
 
+        # Purge
+        if purge:
+            x, rule_pre = cfg_vsrv_rule_get_next (vsrv_pre)
+
+            username = CTK.cfg.get_val ('%s!admin!username'%(PREFIX))
+            password = CTK.cfg.get_val ('%s!admin!password'%(PREFIX))
+            config = PURGE_METHOD_CONF % (locals())
+
+            # Apply the config
+            CTK.cfg.apply_chunk (config)
+            CTK.cfg.normalize ('%s!rule'%(vsrv_pre))
+
         del (CTK.cfg[PREFIX])
         return CTK.cfg_reply_ajax_ok()
 
@@ -101,27 +129,55 @@ class Commit:
 
 class Create:
     def __call__ (self):
-        combo_type = CTK.ComboCfg ('%s!policy'%(PREFIX), trans_options(POLICIES))
+        cont = CTK.Container()
+
+        # Basic Funcionality
+        combo_type = CTK.ComboCfg ('%s!policy'%(PREFIX),    trans_options(POLICIES))
 
         table = CTK.PropsTable()
         table.Add (_('Type of Caching'), combo_type, _(NOTE_POLICY))
 
-        encoded_table = CTK.PropsTable()
+        encoded_table = CTK.PropsTable ()
         encoded_table.Add (_('Extensions'), CTK.TextCfg ('%s!encoded_exts'%(PREFIX), False, {'value': ENCODED_EXTS_DEFAULT}), _(NOTE_ENC_EXTS))
-        encoded_box = CTK.Box({'style':'display:none;'})
+        encoded_box  = CTK.Box ({'style':'display:none;'})
         encoded_box += encoded_table
 
         combo_type.bind ('change',
                          "if ($(this).val() == 'dynamic') {%s} else {%s}"
                          %(encoded_box.JS_to_hide(), encoded_box.JS_to_show()))
 
-        cont = CTK.Container()
-
         submit = CTK.Submitter (URL_APPLY)
         submit += table
         submit += encoded_box
+
+        cont += CTK.RawHTML ("<h2>%s</h2>" %(_("Basic Functionality")))
         cont += submit
 
+        # PURGE
+        check_auth = CTK.CheckCfgText ('%s!purge'%(PREFIX), False, _("Enable"))
+
+        table = CTK.PropsTable()
+        table.Add (_('Enable PURGE requests'), check_auth, _(NOTE_AUTH))
+
+        admin_table = CTK.PropsTable()
+        admin_table.Add (_('Admin Username'), CTK.TextCfg ('%s!admin!username'%(PREFIX), False, {'class':'noauto'}), _(NOTE_ADMIN_USER))
+        admin_table.Add (_('Admin Password'), CTK.TextCfg ('%s!admin!password'%(PREFIX), False, {'class':'noauto'}), _(NOTE_ADMIN_PASSWORD))
+        admin_box  = CTK.Box ({'style':'display:none;'})
+        admin_box += admin_table
+
+        check_auth.bind ('change',
+                         "console.log($(this).is(':checked'));"+
+                         "if ($(this).find(':checked').size() <= 0) {%s} else {%s}"
+                         %(admin_box.JS_to_hide(), admin_box.JS_to_show()))
+
+        submit = CTK.Submitter (URL_APPLY)
+        submit += table
+        submit += admin_box
+
+        cont += CTK.RawHTML ("<h2>%s</h2>" %(_("Purge of Cache Objects")))
+        cont += submit
+
+        # Global Submit
         submit = CTK.Submitter (URL_APPLY)
         submit += CTK.Hidden('final', '1')
         cont += submit
