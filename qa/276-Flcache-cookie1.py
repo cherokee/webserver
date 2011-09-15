da0b1964a5d58f4fe4d5dd832f0c299417377ad4
alobbs <alvaro@alobbs.com>
2011-09-15 11:12:30 +0000

    There were three more Front-Line Cache QA tests that required some changes to run concurrently.
    git-svn-id: svn://cherokee-project.com/cherokee/trunk@6852 5dc97367-97f1-0310-9951-d761b3857238

--- qa/276-Flcache-cookie1.py
+++ qa/276-Flcache-cookie1.py
@@ -1,7 +1,7 @@
+import itertools
 from base import *
 
 DIR     = "flcache-cookie1"
-FILE    = "test.cgi274"
 CONTENT = "Front-line does not cache responses setting cookies"
 
 CONF = """
@@ -22,16 +22,17 @@ echo "%(CONTENT)s"
 """ %(globals())
 
 
-
 class TestEntry (TestBase):
-    def __init__ (self):
+    def __init__ (self, filename):
         TestBase.__init__ (self, __file__)
-        self.request        = "GET /%s/%s HTTP/1.0\r\n" %(DIR, FILE) +\
+        self.request        = "GET /%s/%s HTTP/1.0\r\n" %(DIR, filename) +\
                               "Connection: close\r\n"
         self.expected_error = 200
 
 
 class Test (TestCollection):
+    counter = itertools.count()
+
     def __init__ (self):
         TestCollection.__init__ (self, __file__)
 
@@ -41,13 +42,28 @@ class Test (TestCollection):
         self.delay          = 1
 
     def Prepare (self, www):
-        d = self.Mkdir (www, DIR)
-        self.WriteFile (d, FILE, 0755, CGI_CODE)
+        self.local_dir = self.Mkdir (www, DIR)
+
+    def JustBefore (self, www):
+        test_num = Test.counter.next()
+        self.filename = "test276-id%s-test%s" %(id(self), test_num)
+
+        # Write the new file
+        self.WriteFile (self.local_dir, self.filename, 0755, CGI_CODE)
+
+        # Create sub-request objects
+        self.Empty()
 
         # First request
-        obj = self.Add (TestEntry())
+        obj = self.Add (TestEntry (self.filename))
         obj.expected_content = ['X-Cache: MISS', CONTENT]
 
         # Second request
-        obj = self.Add (TestEntry())
+        obj = self.Add (TestEntry (self.filename))
         obj.expected_content = ['X-Cache: MISS', CONTENT]
+
+    def JustAfter (self, www):
+        # Clean up the local file
+        fp = os.path.join (self.local_dir, self.filename)
+        os.unlink (fp)
+        self.filename = None
--- qa/282-Flcache-expired2.py
+++ qa/282-Flcache-expired2.py
@@ -1,8 +1,8 @@
 import types
+import itertools
 from base import *
 
 DIR     = "flcache-expired2"
-FILE    = "test.cgi"
 CONTENT = "Front-line checks back-end 'Expires' header"
 
 CONF = """
@@ -23,14 +23,16 @@ echo "%(CONTENT)s"
 
 
 class TestEntry (TestBase):
-    def __init__ (self):
+    def __init__ (self, filename):
         TestBase.__init__ (self, __file__)
-        self.request = "GET /%s/%s HTTP/1.0\r\n" %(DIR, FILE) +\
+        self.request = "GET /%s/%s HTTP/1.0\r\n" %(DIR, filename) +\
                        "Connection: close\r\n"
         self.expected_error = 200
 
 
 class Test (TestCollection):
+    counter = itertools.count()
+
     def __init__ (self):
         TestCollection.__init__ (self, __file__)
 
@@ -40,13 +42,28 @@ class Test (TestCollection):
         self.delay          = 1
 
     def Prepare (self, www):
-        d = self.Mkdir (www, DIR)
-        self.WriteFile (d, FILE, 0755, CGI_CODE)
+        self.local_dir = self.Mkdir (www, DIR)
+
+    def JustBefore (self, www):
+        test_num = Test.counter.next()
+        self.filename = "test282-id%s-test%s" %(id(self), test_num)
+
+        # Write the new file
+        self.WriteFile (self.local_dir, self.filename, 0755, CGI_CODE)
+
+        # Create sub-request objects
+        self.Empty()
 
         # Miss
-        obj = self.Add (TestEntry())
+        obj = self.Add (TestEntry (self.filename))
         obj.expected_content = [CONTENT, "X-Cache: MISS"]
 
         # Miss
-        obj = self.Add (TestEntry())
+        obj = self.Add (TestEntry (self.filename))
         obj.expected_content = [CONTENT, "X-Cache: MISS"]
+
+    def JustAfter (self, www):
+        # Clean up the local file
+        fp = os.path.join (self.local_dir, self.filename)
+        os.unlink (fp)
+        self.filename = None
