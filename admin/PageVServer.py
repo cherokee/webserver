99044fc36d8beba15018ef22b133d2e09fc84606
Stefan de Konink <stefan@konink.de>
2011-11-14 02:55:24 +0100

    HSTS admin fixup; I guess it is not very good if "One Year" is part of the config.

--- admin/PageVServer.py
+++ admin/PageVServer.py
@@ -685,8 +685,8 @@ class SecutiryWidgetContent (CTK.Container):
         table.Add (_('Enable HSTS'), CTK.CheckCfgText ('%s!hsts'%(pre), False, _('Accept')), _(NOTE_HSTS))
 
         if int(CTK.cfg.get_val('%s!hsts' %(pre), "0")):
-            table.Add (_('HSTS Max-Age'),       CTK.TextCfg ('%s!hsts!max_age'%(pre), True, {'optional_string':_("One year")}), _(NOTE_HSTS_MAXAGE))
-            table.Add (_('Include Subdomains'), CTK.CheckCfgText ('%s!subdomains'%(pre), True, _('Include all')), _(NOTE_HSTS_SUBDOMAINS))
+            table.Add (_('HSTS Max-Age'),       CTK.TextCfg      ('%s!hsts!max_age'   %(pre), True, {'optional_string':_("31536000")}), _(NOTE_HSTS_MAXAGE))
+            table.Add (_('Include Subdomains'), CTK.CheckCfgText ('%s!hsts!subdomains'%(pre), True, _('Include all')), _(NOTE_HSTS_SUBDOMAINS))
 
         submit = CTK.Submitter (url_apply)
         submit.bind ('submit_success', refreshable.JS_to_refresh())
