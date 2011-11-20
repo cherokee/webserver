4536bb74cc0f46c51eca697937665844bac7498f
Alvaro Lopez Ortega <alvaro@alobbs.com>
2011-11-20 23:05:59 +0100

    Fixes the HSTS QA entries. They work with SSL now.

--- qa/293-HSTS-subdomains1.py
+++ qa/293-HSTS-subdomains1.py
@@ -11,13 +11,23 @@ vserver!2930!rule!1!match = default
 vserver!2930!rule!1!handler = dirlist
 """
 
+# 6.1.  HTTP-over-Secure-Transport Request Type
+#
+#   When replying to an HTTP request that was conveyed over a secure
+#   transport, a HSTS Host SHOULD include in its response message a
+#   Strict-Transport-Security HTTP Response Header that MUST satisfy the
+#   grammar specified above in Section 5.1 "Strict-Transport-Security
+#   HTTP Response Header Field".  If a Strict-Transport-Security HTTP
+#   Response Header is included, the HSTS Host MUST include only one such
+#   header.
+
 class Test (TestBase):
     def __init__ (self):
         TestBase.__init__ (self, __file__)
-        self.name             = "HSTS: with Subdomains"
-        self.request          = "HTTP / HTTP/1.0\r\n" + \
+        self.name             = "HSTS over SSL"
+        self.request          = "GET / HTTP/1.0\r\n" + \
                                 "Host: %s\r\n" %(NICK)
-        self.expected_error   = 301
+        self.expected_error   = 200
         self.expected_content = ["Strict-Transport-Security:", "includeSubdomains"]
 
     def Prepare (self, www):
@@ -26,3 +36,6 @@ class Test (TestBase):
         vars = globals()
         vars.update(locals())
         self.conf = CONF %(vars)
+
+    def Precondition (self):
+        return self.is_ssl
