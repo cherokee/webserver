806fcfd3d7ba0296bae47276170bd18d01cd0def
Stefan de Konink <stefan@konink.de>
2012-08-11 11:55:18 +0200

    Applied the fastcgi + flup fix from Issue #11 by sciyoshi.

--- cherokee/handler_fcgi.c
+++ cherokee/handler_fcgi.c
@@ -516,7 +516,7 @@ build_header (cherokee_handler_fcgi_t *hdl, cherokee_buffer_t *buffer)
 
 	/* No POST?
 	 */
-	if ((! http_method_with_input (conn->header.method)) || (! conn->post.has_info)) {
+	if ((! http_method_with_input (conn->header.method)) || (! conn->post.has_info) || (! conn->post.len)) {
 		TRACE (ENTRIES",post", "Post: %s\n", "has no post");
 		add_empty_packet (hdl, FCGI_STDIN);
 	}
