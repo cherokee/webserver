5c5877a5cd8c188d3baa044331411206348682ac
Stefan de Konink <stefan@konink.de>
2014-01-01 15:11:31 +0100

    A created connection to an ldap server should be closed after use.
    Fix #904

--- cherokee/validator_ldap.c
+++ cherokee/validator_ldap.c
@@ -253,6 +253,10 @@ ret_t
 cherokee_validator_ldap_free (cherokee_validator_ldap_t *ldap)
 {
 	cherokee_buffer_mrproper (&ldap->filter);
+
+	if (ldap->conn)
+		ldap_unbind (ldap->conn);
+
 	return ret_ok;
 }
 
