689204476775bee05ed4a292c2891de3c9ec6a4d
Stefan de Konink <stefan@konink.de>
2014-09-17 23:20:00 +0200

    Maintain keep-alive for replies without body. Fix #1140
    Cherokee closed the keep-alive connection for 204 No Content and
    304 Not Modified replies. As you may understand especially the latter
    has a serious performance implication in real life situation. This patch
    addresses that.

--- cherokee/connection.c
+++ cherokee/connection.c
@@ -996,7 +996,16 @@ cherokee_connection_build_header (cherokee_connection_t *conn)
 	 * has to be turned off.
 	 */
 	if (conn->keepalive != 0) {
-		if (! HANDLER_SUPPORTS (conn->handler, hsupport_length)) {
+		/* A request that contains a body, is required to provide
+		 * a Content-Length header to maintain Keep-Alive.
+		 * Alternatively chucked_encoding may be used.
+		 *
+		 * According to RFC 2616 redirections SHOULD contain
+		 * a body, so they MAY require a Content-Length header.
+		 */
+		if (! HANDLER_SUPPORTS (conn->handler, hsupport_length) &&
+		      http_code_with_body (conn->error_code)
+		) {
 			if (! conn->chunked_encoding) {
 				conn->keepalive = 0;
 			}
